c5ecd9ee57a0b658ed81138dff5f4c2ca3191659
Julien Perret <Julien.perret@gmail.com>
2009-05-13 13:49:15 +0000

    Suppression d'une dépendance locale

--- src/fr/ign/cogit/geoxygene/example/TestShapefileReader.java
+++ src/fr/ign/cogit/geoxygene/example/TestShapefileReader.java
@@ -1,8 +1,11 @@
 package fr.ign.cogit.geoxygene.example;
 
+import java.io.File;
+
 import fr.ign.cogit.geoxygene.feature.DefaultFeature;
 import fr.ign.cogit.geoxygene.feature.Population;
 import fr.ign.cogit.geoxygene.util.conversion.ShapefileReader;
+import fr.ign.cogit.geoxygene.util.loader.gui.GUIShapefileChoice;
 import fr.ign.cogit.geoxygene.util.viewer.ObjectViewer;
 
 public class TestShapefileReader {
@@ -12,8 +15,16 @@ public class TestShapefileReader {
      */
     public static void main(String[] args) {
 	ObjectViewer vwr = new ObjectViewer();
-	Population<DefaultFeature> pop = ShapefileReader.read("/home/jperret/Data/GeOpenSim/Zone4/batiment_Z4/batiment_2002_Z4.shp");
-	vwr.addFeatureCollection(pop, "fichier");
+	GUIShapefileChoice chooser = new GUIShapefileChoice(true);
+	File[] files = chooser.getSelectedFiles();
+	chooser.dispose();
+	if (files!=null) {
+	    for (int i = 0 ; i < files.length ; i++) {
+		System.out.println(files[i].getAbsolutePath());
+		Population<DefaultFeature> pop = ShapefileReader.read(files[i].getAbsolutePath());
+		vwr.addFeatureCollection(pop, files[i].getName().substring(0, files[i].getName().lastIndexOf(".")));
+	    }
+	}
     }
 
 }
