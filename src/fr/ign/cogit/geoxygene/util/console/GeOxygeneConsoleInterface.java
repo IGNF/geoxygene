bfb75b5ffa2bc74740b104ca5d759a9885ee56e2
Julien Perret <Julien.perret@gmail.com>
2010-01-05 12:12:22 +0000

    Documentation, Internationalization and style correction.

--- src/fr/ign/cogit/geoxygene/util/console/GeOxygeneConsoleInterface.java
+++ src/fr/ign/cogit/geoxygene/util/console/GeOxygeneConsoleInterface.java
@@ -1,27 +1,22 @@
 /*
  * This file is part of the GeOxygene project source files.
- * 
- * GeOxygene aims at providing an open framework which implements OGC/ISO specifications for
- * the development and deployment of geographic (GIS) applications. It is a open source
- * contribution of the COGIT laboratory at the Institut Géographique National (the French
- * National Mapping Agency).
- * 
+ * GeOxygene aims at providing an open framework which implements OGC/ISO
+ * specifications for the development and deployment of geographic (GIS)
+ * applications. It is a open source contribution of the COGIT laboratory at
+ * the Institut Géographique National (the French National Mapping Agency).
  * See: http://oxygene-project.sourceforge.net
- * 
  * Copyright (C) 2005 Institut Géographique National
- *
- * This library is free software; you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software Foundation;
- * either version 2.1 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with
- * this library (see file LICENSE if present); if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or any later
+ * version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details. You should have received a copy of the GNU Lesser General
+ * Public License along with this library (see file LICENSE if present); if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
  */
 
 package fr.ign.cogit.geoxygene.util.console;
@@ -39,15 +34,10 @@ import javax.swing.JPanel;
 /**
  *
  * @author Thierry Badard & Arnaud Braun & Eric Grosso
- * @version 1.1
- * 
  */
-
 class GeOxygeneConsoleInterface extends JFrame {
-
-
 	/**
-	 * 
+	 * serial uid.
 	 */
 	private static final long serialVersionUID = 1L;
 	private static String ojb = "OJB"; //$NON-NLS-1$
@@ -158,5 +148,4 @@ class GeOxygeneConsoleInterface extends JFrame {
 		else if (string.equals(castor)) return GeOxygeneConsole.CASTOR;
 		else return 0;
 	}
-
 }
