d00bd5aa2a726bd9f4aa96501000ee6f86a83e89
Julien Perret <Julien.perret@gmail.com>
2010-08-04 12:53:32 +0000

    Adding a few images and illustrations in SLDDemoApplication

--- src/fr/ign/cogit/geoxygene/appli/SLDDemoApplication.java
+++ src/fr/ign/cogit/geoxygene/appli/SLDDemoApplication.java
@@ -23,19 +23,26 @@ package fr.ign.cogit.geoxygene.appli;
 
 import java.awt.Color;
 import java.awt.geom.NoninvertibleTransformException;
+import java.net.URL;
+import java.util.Arrays;
 
 import fr.ign.cogit.geoxygene.feature.DataSet;
 import fr.ign.cogit.geoxygene.feature.DefaultFeature;
 import fr.ign.cogit.geoxygene.feature.Population;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPosition;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPositionList;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_Envelope;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_LineString;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_Polygon;
+import fr.ign.cogit.geoxygene.style.Displacement;
 import fr.ign.cogit.geoxygene.style.ExternalGraphic;
-import fr.ign.cogit.geoxygene.style.Fill;
 import fr.ign.cogit.geoxygene.style.Graphic;
 import fr.ign.cogit.geoxygene.style.GraphicFill;
+import fr.ign.cogit.geoxygene.style.GraphicStroke;
 import fr.ign.cogit.geoxygene.style.Layer;
-import fr.ign.cogit.geoxygene.style.Mark;
+import fr.ign.cogit.geoxygene.style.LineSymbolizer;
 import fr.ign.cogit.geoxygene.style.PolygonSymbolizer;
+import fr.ign.cogit.geoxygene.style.Shadow;
 
 /**
  * Base class for GeOxygene applications.
@@ -53,34 +60,21 @@ public class SLDDemoApplication extends GeOxygeneApplication {
         splashScreen.setVisible(true);
         SLDDemoApplication application = new SLDDemoApplication();
         ProjectFrame projectFrame = application.getFrame().newProjectFrame();
+
         Layer layer = projectFrame.getSld().createLayer("Vegetation", //$NON-NLS-1$
                 GM_Polygon.class, new Color(0.5f, 1.f, 0.5f), Color.green,
                 1f, 4);
         PolygonSymbolizer symbolizer = (PolygonSymbolizer) layer.getSymbolizer();
         GraphicFill graphicFill = new GraphicFill();
-        Graphic graphicCircle = new Graphic();
-        Mark mark = new Mark();
-        mark.setWellKnownName("circle"); //$NON-NLS-1$
-        graphicCircle.setSize(20f);
-        Fill fill = new Fill();
-        fill.setColor(new Color(0.4f,1.f,0.4f));
-        mark.setFill(fill);
-        graphicCircle.getMarks().add(mark);
-        //graphicFill.getGraphics().add(graphicCircle);
-        Graphic graphicStar = new Graphic();
-        Mark markStar = new Mark();
-        markStar.setWellKnownName("star"); //$NON-NLS-1$
-        graphicStar.setSize(10f);
-        Fill fillStar = new Fill();
-        fillStar.setColor(new Color(1.f,0.4f,0.4f));
-        markStar.setFill(fillStar);
-        graphicStar.getMarks().add(markStar);
-        graphicFill.getGraphics().add(graphicStar);
         Graphic graphicTree = new Graphic();
-        graphicTree.setSize(30);
+        graphicTree.setSize(100);
         ExternalGraphic tree = new ExternalGraphic();
-        tree.setHref("http://recherche.ign.fr/labos/cogit/img/LOGO_COGIT.gif"); //$NON-NLS-1$
-        tree.setFormat("gif");
+        //tree.setHref("http://recherche.ign.fr/labos/cogit/img/LOGO_COGIT.gif"); //$NON-NLS-1$
+        //tree.setFormat("gif"); //$NON-NLS-1$
+        URL url = SLDDemoApplication.class.getResource("/images/herbes.png"); //$NON-NLS-1$
+        System.out.println(url);
+        tree.setHref(url.toString());
+        tree.setFormat("png"); //$NON-NLS-1$
         graphicTree.getExternalGraphics().add(tree);
         graphicFill.getGraphics().add(graphicTree);
         symbolizer.getFill().setGraphicFill(graphicFill);
@@ -88,6 +82,96 @@ public class SLDDemoApplication extends GeOxygeneApplication {
         pop.add(new DefaultFeature(new GM_Polygon(new GM_Envelope(0, 100, 0, 100))));
         DataSet.getInstance().addPopulation(pop);
         projectFrame.addLayer(layer);
+
+        Layer layer2 = projectFrame.getSld().createLayer("Batiment", //$NON-NLS-1$
+                    GM_Polygon.class, Color.blue, Color.gray,
+                    1f, 2);
+        PolygonSymbolizer symbolizer2 = (PolygonSymbolizer) layer2.getSymbolizer();
+        Shadow shadow = new Shadow();
+        shadow.setColor(Color.black);
+        Displacement d = new Displacement();
+        d.setDisplacementX(5);
+        d.setDisplacementY(-5);
+        shadow.setDisplacement(d);
+        symbolizer2.setShadow(shadow);
+        Population<DefaultFeature> pop2 = new Population<DefaultFeature>("Batiment"); //$NON-NLS-1$
+        pop2.add(new DefaultFeature(new GM_Polygon(new GM_Envelope(110, 200, 110, 200))));
+        DataSet.getInstance().addPopulation(pop2);
+        projectFrame.addLayer(layer2);
+
+        Layer layer3 = projectFrame.getSld().createLayer("Route", //$NON-NLS-1$
+                    GM_LineString.class, Color.red, Color.red,
+                    1f, 4);
+        LineSymbolizer symbolizer3 = (LineSymbolizer) layer3.getSymbolizer();
+        
+        GraphicStroke graphicStroke = new GraphicStroke();
+        Graphic graphicCircle = new Graphic();
+        graphicCircle.setSize(10);
+        ExternalGraphic externalGraphicCircle = new ExternalGraphic();
+        URL urlCircle = SLDDemoApplication.class.getResource("/images/circle.png"); //$NON-NLS-1$
+        externalGraphicCircle.setHref(urlCircle.toString());
+        externalGraphicCircle.setFormat("png"); //$NON-NLS-1$
+        graphicCircle.getExternalGraphics().add(externalGraphicCircle);
+        graphicStroke.getGraphics().add(graphicCircle);
+
+        /*
+        Graphic graphicStar = new Graphic();
+        Mark markStar = new Mark();
+        markStar.setWellKnownName("star"); //$NON-NLS-1$
+        graphicStar.setSize(20f);
+        Fill fillStar = new Fill();
+        fillStar.setColor(new Color(1.f,0.4f,0.4f));
+        markStar.setFill(fillStar);
+        graphicStar.getMarks().add(markStar);
+        graphicStroke.getGraphics().add(graphicStar);
+        */
+        symbolizer3.getStroke().setGraphicType(graphicStroke);
+
+        Population<DefaultFeature> pop3 = new Population<DefaultFeature>("Route"); //$NON-NLS-1$
+        pop3.add(new DefaultFeature(new GM_LineString(new DirectPositionList(Arrays.asList(
+                    new DirectPosition(110,0),
+                    new DirectPosition(110,100),
+                    new DirectPosition(200,100)
+                    )))));
+        DataSet.getInstance().addPopulation(pop3);
+        projectFrame.addLayer(layer3);
+
+        Layer layer4 = projectFrame.getSld().createLayer("Chemin", //$NON-NLS-1$
+                    GM_LineString.class, Color.red, Color.red,
+                    1f, 4);
+        LineSymbolizer symbolizer4 = (LineSymbolizer) layer4.getSymbolizer();
+        GraphicFill graphicFill2 = new GraphicFill();
+        Graphic graphicCircles = new Graphic();
+        graphicCircles.setSize(10);
+        ExternalGraphic externalGraphic = new ExternalGraphic();
+        //tree.setHref("http://recherche.ign.fr/labos/cogit/img/LOGO_COGIT.gif"); //$NON-NLS-1$
+        //tree.setFormat("gif"); //$NON-NLS-1$
+        URL urlCircles = SLDDemoApplication.class.getResource("/images/circles.png"); //$NON-NLS-1$
+        externalGraphic.setHref(urlCircles.toString());
+        externalGraphic.setFormat("png"); //$NON-NLS-1$
+        graphicCircles.getExternalGraphics().add(externalGraphic);
+        graphicFill2.getGraphics().add(graphicCircles);
+        /*
+        Mark mark = new Mark();
+        mark.setWellKnownName("circle"); //$NON-NLS-1$
+        graphicCircle.setSize(10f);
+        Fill fill = new Fill();
+        fill.setColor(new Color(0.8f,0.2f,0.4f));
+        mark.setFill(fill);
+        graphicCircle.getMarks().add(mark);
+        graphicFill2.getGraphics().add(graphicCircle);
+        */
+        symbolizer4.getStroke().setGraphicType(graphicFill2);
+
+        Population<DefaultFeature> pop4 = new Population<DefaultFeature>("Chemin"); //$NON-NLS-1$
+        pop4.add(new DefaultFeature(new GM_LineString(new DirectPositionList(Arrays.asList(
+                    new DirectPosition(0,110),
+                    new DirectPosition(100,110),
+                    new DirectPosition(100,200)
+                    )))));
+        DataSet.getInstance().addPopulation(pop4);
+        projectFrame.addLayer(layer4);
+
         try {
             projectFrame.getLayerViewPanel().getViewport().zoomToFullExtent();
         } catch (NoninvertibleTransformException e) {
