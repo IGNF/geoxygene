3dcbb2919b9be31e88fe8ad0c2672a9554d20fd6
Charlotte Hoarau <charlotte.hoarau@gmail.com>
2010-12-03 17:06:46 +0000

    Improvements in the COGITColorChooserPanel:   - Adding more information about the colors (sRGB and Hexa codes)   - Removing the useless buttons and components (image sample)   - Cleaning of the code   - Internationalization of the strings

--- src/fr/ign/cogit/geoxygene/appli/COGITColorChooserPanel.java
+++ src/fr/ign/cogit/geoxygene/appli/COGITColorChooserPanel.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
@@ -19,7 +18,6 @@ import javax.swing.JColorChooser;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.colorchooser.AbstractColorChooserPanel;
 import javax.swing.table.TableColumn;
@@ -29,145 +27,99 @@ import fr.ign.cogit.geoxygene.style.colorimetry.ColorReferenceSystem;
 import fr.ign.cogit.geoxygene.style.colorimetry.ColorimetricColor;
 
 /**
+ * Color Chooser providing the colors of COGIT Laboratory color wheels.
+ * 
  * @author Charlotte Hoarau
  *
  */
-public class COGITColorChooserPanel extends AbstractColorChooserPanel
+public class COGITColorChooserPanel extends AbstractColorChooserPanel 
 									implements MouseListener{
 	private static final long serialVersionUID = 1L;
 
 	JLabel lblCerclesImage;
-	JLabel lblColorImage;
-	BufferedImage sampleImage;
-	JPanel panneau;
+	JPanel mainPanel;
 	JTable tCodesCouleur;
-	JScrollPane panneauTab;
-	JPanel panneauBtn;
-	JButton btnOK;
-	JButton btnCancel;
+	JPanel tablePanel;
 	BufferedImage cerclesImage;
-
+	
 	public COGITColorChooserPanel(){
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		setTitle("Cercles Chromatiques Lucil");
-
+		
 		cerclesImage = createCercleImage();
 		lblCerclesImage = new JLabel(new ImageIcon(cerclesImage));
-		lblCerclesImage.setName(new String("Label contenant l'image des cercles chromatiques"));
-		lblCerclesImage.addMouseListener(this);
-
-		tCodesCouleur = new JTable(3,6);
-		tCodesCouleur.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		lblCerclesImage.addMouseListener(this);		
+		
+		tCodesCouleur = new JTable(4,8);
+		Font f = tCodesCouleur.getFont();
+	    f = f.deriveFont(Font.BOLD);
+	    tCodesCouleur.setFont(f);
+		tCodesCouleur.setAutoResizeMode(JTable.AUTO_RESIZE_OFF); 
 		tCodesCouleur.setGridColor(new  Color(225,225,225));//Couleur des bords
 		tCodesCouleur.setBackground(new Color(200,200,200));//Couleur des cases
 		tCodesCouleur.setForeground(new Color(100,100,100));//Couleur du texte
-
-		tCodesCouleur.setValueAt("Couleur Usuelle",0,0);
-		tCodesCouleur.setValueAt("Niveau de valeur",1,0);
-		tCodesCouleur.setValueAt("Clé Couleur",2,0);
-
-		tCodesCouleur.setValueAt("Rouge",0,2);
-		tCodesCouleur.setValueAt("Vert",1,2);
-		tCodesCouleur.setValueAt("Bleu",2,2);
-
-		tCodesCouleur.setValueAt("L",0,4);
-		tCodesCouleur.setValueAt("a",1,4);
-		tCodesCouleur.setValueAt("b",2,4);
-		Font f = tCodesCouleur.getFont();
-		//f = f.deriveFont((float)20);
-	    f = f.deriveFont(Font.BOLD);
-	    tCodesCouleur.setFont(f);
-
+		
+		tCodesCouleur.setValueAt("COGIT",0,0);
+		
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.UsualColor"),1,0); //$NON-NLS-1$
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.Lightness"),2,0); //$NON-NLS-1$
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.ColorKey"),3,0); //$NON-NLS-1$
+		
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.Others"),0,2); //$NON-NLS-1$
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.Hexa"),1,2); //$NON-NLS-1$
+		tCodesCouleur.setValueAt("sRGB",2,2);
+		
+		tCodesCouleur.setValueAt("RGB",0,4);
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.Red"),1,4); //$NON-NLS-1$
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.Green"),2,4); //$NON-NLS-1$
+		tCodesCouleur.setValueAt(I18N.getString(
+				"COGITColorChooserPanel.Blue"),3,4); //$NON-NLS-1$
+		
+		tCodesCouleur.setValueAt("CIELab",0,6);
+		tCodesCouleur.setValueAt("L",1,6);
+		tCodesCouleur.setValueAt("a",2,6);
+		tCodesCouleur.setValueAt("b",3,6);
+		
 		for (int i=0;i<6;i++){
 			TableColumn colonne = tCodesCouleur.getColumnModel().getColumn(i);
-
-			if (i==3 || i==5){
-				colonne.setPreferredWidth(30);
-				colonne.setMinWidth(30);
-				colonne.setMaxWidth(30);
-			}else if (i==2 || i==4){
-				colonne.setPreferredWidth(60);
-				colonne.setMinWidth(60);
-				colonne.setMaxWidth(60);
-				//tCodesCouleur.setFont(new Font());
+			
+			if (i==0){
+				colonne.setPreferredWidth(110);
+				colonne.setMinWidth(110);
+				colonne.setMaxWidth(110);
+			}else if (i==4 || i==6){
+				colonne.setPreferredWidth(50);
+				colonne.setMinWidth(50);
+				colonne.setMaxWidth(50);
 			}else if (i==1){
-				colonne.setPreferredWidth(150);
-				colonne.setMinWidth(200);
-				colonne.setMaxWidth(200);
+				colonne.setPreferredWidth(180);
+				colonne.setMinWidth(180);
+				colonne.setMaxWidth(180);
 			}else{
-				colonne.setPreferredWidth(100);
-				colonne.setMinWidth(100);
-				colonne.setMaxWidth(100);
+				colonne.setPreferredWidth(90);
+				colonne.setMinWidth(90);
+				colonne.setMaxWidth(90); 
 			}
 		}
-
-		//Initialization of the sample image with a gray rectangle.
-		sampleImage = new BufferedImage(
-				115,
-				85,
-				java.awt.image.BufferedImage.TYPE_INT_RGB);
-		lblColorImage = new JLabel(new ImageIcon(sampleImage));
-		createSampleImage(200, 200, 200, lblColorImage);
-		lblColorImage.setVisible(false);
-
+		
 		//Initialization of the buttons
-		btnOK = new JButton("OK");
-		btnCancel = new JButton("Annuler");
-		panneauBtn = new JPanel();
-		panneauBtn.setBackground(new Color(225,225,225));
-		panneauBtn.setName("Panneau contenant les boutons");
-		panneauBtn.add(tCodesCouleur);
-		panneauBtn.add(lblColorImage);
-		panneauBtn.add(btnOK);
-		panneauBtn.add(btnCancel);
-
-		panneau = new JPanel();
-		panneau.setLayout(new BorderLayout());
-		panneau.setName("Panneau principal");
-		panneau.add(lblCerclesImage, BorderLayout.NORTH);
-		panneau.add(panneauBtn, BorderLayout.SOUTH);
-
-		add(panneau);
+		tablePanel = new JPanel();
+		tablePanel.setBackground(new Color(225,225,225));
+		tablePanel.add(tCodesCouleur);
+		
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(lblCerclesImage, BorderLayout.NORTH);
+		mainPanel.add(tablePanel, BorderLayout.SOUTH);
+		
+		add(mainPanel);
 	}
-
-	public Graphics createSampleImage(int r, int v, int b, JLabel lbl){
-
-		Graphics g = lbl.getGraphics();
-
-		if(g==null){
-			g = sampleImage.createGraphics();
-		}
-
-		g.setColor(new Color(130,130,130));
-		g.fillRect(0, 0, 115, 85);
-
-		g.setColor(Color.BLACK);
-		g.fillRect(75,45,25,25);
-		g.fillRect(45,15,25,25);
-
-		g.setColor(Color.WHITE);
-		g.fillRect(45,45,25,25);
-		g.fillRect(15,15,25,25);
-		g.fillRect(75,15,25,25);
-
-		g.setColor(new Color(r,v,b));
-		g.fillRect(15, 45, 25, 25);
-		g.fillRect(50, 50, 15, 15);
-		g.fillRect(80, 50, 15, 15);
-		g.fillRect(20, 20, 15, 15);
-		g.fillRect(50, 20, 15, 15);
-		g.fillRect(80, 20, 15, 15);
-
-		g.setColor(Color.BLACK);
-		g.fillRect(55,25,5,5);
-		g.fillRect(85,25,5,5);
-
-		g.setColor(Color.WHITE);
-		g.fillRect(25,25,5,5);
-
-		return g;
-	}
-
+	
 	public BufferedImage createCercleImage(){
 		BufferedImage cerclesImage =
 			new BufferedImage(1100,450,java.awt.image.BufferedImage.TYPE_INT_RGB);
@@ -176,13 +128,14 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 			(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(new Color(225,225,225));
 		g.fillRect(0, 0, 1100, 450);
-
+		
 		ColorReferenceSystem crs = ColorReferenceSystem.unmarshall(
-				ColorReferenceSystem.class.getResource("/color/ColorReferenceSystem.xml").getPath()); //$NON-NLS-1$
-
+				ColorReferenceSystem.class.getResource(
+						"/color/ColorReferenceSystem.xml").getPath()); //$NON-NLS-1$
+		
 		//Création de l'image du cercle principal (Couleurs pures)
 		for (int j=0;j<12;j++){
-			List<ColorimetricColor> listCouleurs = crs.getSlice(0, j);
+			List<ColorimetricColor> listCouleurs = crs.getSlice(0, j);	
 			for (int i=0;i<listCouleurs.size();i++){
 				ColorimetricColor couleur = listCouleurs.get(listCouleurs.size()-1-i);
 				g.setColor(
@@ -193,10 +146,10 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 				g.fillArc(50+i*15, 50+i*15, 300-30*i, 300-30*i, 30*(j+1), 30);
 			}
 		}
-
+		
 		//Création de l'image du cercle des Couleurs grisées
 		for (int j=0;j<7;j++){
-			List<ColorimetricColor> listCouleurs = crs.getSlice(1,j);
+			List<ColorimetricColor> listCouleurs = crs.getSlice(1,j);	
 			for (int i=0;i<listCouleurs.size();i++){
 				ColorimetricColor couleur = listCouleurs.get(listCouleurs.size()-1-i);
 				g.setColor(
@@ -206,11 +159,11 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 								couleur.getBlueRGB()));
 				g.fillArc(400+i*15, 50+i*15, 300-30*i, 300-30*i, 52*j, 52);
 			}
-		}
-
+		}	
+		
 		//Création de l'image du cercle des Gris colorés
 		for (int j=0;j<7;j++){
-			List<ColorimetricColor> listCouleurs = crs.getSlice(2,j);
+			List<ColorimetricColor> listCouleurs = crs.getSlice(2,j);	
 			for (int i=0;i<listCouleurs.size();i++){
 				ColorimetricColor couleur = listCouleurs.get(listCouleurs.size()-1-i);
 				g.setColor(
@@ -221,9 +174,9 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 				g.fillArc(750+i*15, 50+i*15, 300-30*i, 300-30*i, 52*j, 52);
 			}
 		}
-
+		
 		//Création de l'image de la gamme de Gris, Noir et Blanc
-		List<ColorimetricColor> listCouleurs = crs.getSlice(3,0);
+		List<ColorimetricColor> listCouleurs = crs.getSlice(3,0);	
 		for (int i=0;i<listCouleurs.size();i++){
 			ColorimetricColor couleur = listCouleurs.get(listCouleurs.size()-1-i);
 			g.setColor(
@@ -233,9 +186,9 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 							couleur.getBlueRGB()));
 			g.fillRect(550+i*40, 400, 40, 25);
 		}
-
+		
 		//Création de l'image de la gamme de Marrons
-		List<ColorimetricColor> listCouleursM = crs.getSlice(3,1);
+		List<ColorimetricColor> listCouleursM = crs.getSlice(3,1);	
 		for (int i=0;i<listCouleursM.size();i++){
 			ColorimetricColor couleur = listCouleursM.get(listCouleursM.size()-1-i);
 			g.setColor(
@@ -252,17 +205,17 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 		g.fillArc(155, 155, 90, 90, 0, 360);
 		g.fillArc(460, 110, 180, 180, 0, 360);
 		g.fillArc(810, 110, 180, 180, 0, 360);
-
+		
 		g.setColor(new Color(200,200,200));
 		g.drawRect(220, 400, 40*7, 25);
 		g.drawRect(550, 400, 40*9, 25);
 		g.drawArc(155, 155, 90, 90, 0, 360);
 		g.drawArc(460, 110, 180, 180, 0, 360);
 		g.drawArc(810, 110, 180, 180, 0, 360);
-
+		
 		return cerclesImage;
 	}
-
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if (e.getSource() == lblCerclesImage){
@@ -274,7 +227,7 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 		    updateTable(newColor);
 		}
 	}
-
+	
 	public void updateTable(ColorimetricColor c){
 
 		float[] labCodes = new float[3];
@@ -285,25 +238,25 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 				c.getGreenRGB() == 225 ||
 				c.getBlueRGB() == 225){
 		}else{
-			tCodesCouleur.setValueAt(c.getUsualName(),0,1);
-			tCodesCouleur.setValueAt(c.getLightness(),1,1);
-			tCodesCouleur.setValueAt(c.getCleCoul(),2,1);
-			tCodesCouleur.setValueAt(c.getRedRGB(),0,3);
-			tCodesCouleur.setValueAt(c.getGreenRGB(), 1,3);
-			tCodesCouleur.setValueAt(c.getBlueRGB(), 2,3);
-			tCodesCouleur.setValueAt(Math.round(labCodes[0]),0,5);
-			tCodesCouleur.setValueAt(Math.round(labCodes[1]), 1,5);
-			tCodesCouleur.setValueAt(Math.round(labCodes[2]), 2,5);
-
-			createSampleImage(c.getRedRGB(),
-					c.getGreenRGB(),
-					c.getBlueRGB(),
-					lblColorImage);
+			tCodesCouleur.setValueAt(c.getUsualName(),1,1);
+			tCodesCouleur.setValueAt(c.getLightness(),2,1);
+			tCodesCouleur.setValueAt(c.getCleCoul(),3,1);
+			
+			tCodesCouleur.setValueAt(Integer.toHexString(c.toColor().getRGB()),1,3);
+			tCodesCouleur.setValueAt(c.toColor().getRGB(),2,3);
+			
+			tCodesCouleur.setValueAt(c.getRedRGB(),   1,5);
+			tCodesCouleur.setValueAt(c.getGreenRGB(), 2,5);
+			tCodesCouleur.setValueAt(c.getBlueRGB(),  3,5);
+			
+			tCodesCouleur.setValueAt(Math.round(labCodes[0]), 1, 7);
+			tCodesCouleur.setValueAt(Math.round(labCodes[1]), 2, 7);
+			tCodesCouleur.setValueAt(Math.round(labCodes[2]), 3, 7);
 		}
-
+		
 		getColorSelectionModel().setSelectedColor(c.toColor());
 	}
-
+	
 	@Override
 	public void mouseEntered(MouseEvent arg0) {}
 
@@ -315,7 +268,7 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {}
-
+	
 	/**
 	 * @param args
 	 */
@@ -326,26 +279,28 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 	}
 
 	/**
-	 * Creates and returns a new dialog containing the specified ColorChooser pane
-	 * along with "OK", "Cancel", and "Reset" buttons.
-	 * If the "OK" or "Cancel" buttons are pressed, the dialog is automatically hidden (but not disposed).
-	 * If the "Reset" button is pressed, the color-chooser's color will be reset to the color
-	 * which was set the last time show was invoked on the dialog and the dialog will remain showing.
+	 * Creates and returns a new dialog containing the specified ColorChooser
+	 * pane along with "OK", "Cancel", and "Reset" buttons. 
+	 * If the "OK" or "Cancel" buttons are pressed,
+	 * the dialog is automatically hidden (but not disposed). 
+	 * If the "Reset" button is pressed,
+	 * the color-chooser's color will be reset to the color which was set the
+	 * last time show was invoked on the dialog and the dialog will remain showing. 
 	 * @param component
 	 * @param initialColor
-	 * @return The chosen color
+	 * @return
 	 */
 	public static Color showDialog(Component component, String title, Color initialColor){
 		JColorChooser colorChooser = new JColorChooser(initialColor != null?
                 initialColor : Color.white);
-
+		
 		colorChooser.addChooserPanel(new COGITColorChooserPanel());
 
 		JDialog dialog = JColorChooser.createDialog(
 				component, title, true, colorChooser, null, null);
 		dialog.setVisible(true);
 		Color c = colorChooser.getColor();
-
+		
 		return c;
 	}
 	@Override
@@ -354,7 +309,8 @@ public class COGITColorChooserPanel extends AbstractColorChooserPanel
 
 	@Override
 	public String getDisplayName() {
-		return "COGIT Color Reference System";
+		return I18N.getString(
+				"COGITColorChooserPanel.COGITColorReferenceSystem"); //$NON-NLS-1$
 	}
 
 	@Override
