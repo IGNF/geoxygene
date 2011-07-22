8511e68b31edc70db53f2de3b7bbbdfa4e4e6894
Julien Perret <Julien.perret@gmail.com>
2011-07-22 16:11:24 +0000

    Added new methods for minimum bounding rectangle to JtsAlgorithms.

--- src/fr/ign/cogit/geoxygene/util/algo/JtsAlgorithms.java
+++ src/fr/ign/cogit/geoxygene/util/algo/JtsAlgorithms.java
@@ -1636,6 +1636,33 @@ public class JtsAlgorithms implements GeomAlgorithms {
     return result;
   }
   
+
+  /**
+   * Plus Petit Rectangle Englobant d'une géométrie préservant son aire.
+   * Smallest Enclosing Rectangle of a geometry preserving its area.
+   * 
+   * @param geom une géométrie, a geometry
+   * @return le Plus Petit Rectangle Englobant, the Smallest Enclosing Rectangle
+   */
+  public static Polygon MBRAirePreservee(Geometry geom){
+      Polygon out=MBR(geom);
+      return homothetie(out, (float) Math.sqrt(geom.getArea()/out.getArea()));
+  }
+
+
+  /**
+   * Plus Petit Rectangle Englobant d'une géométrie respectant un aire donnée.
+   * Smallest Enclosing Rectangle of a geometry with a given area.
+   * 
+   * @param geom une géométrie, a geometry
+   * @param aireCible aire visée, target area
+   * @return le Plus Petit Rectangle Englobant, the Smallest Enclosing Rectangle
+   */
+  public static Polygon MBRAireCible(Geometry geom, double aireCible){
+      Polygon out=MBR(geom);
+      return homothetie(out, (float) Math.sqrt(aireCible/out.getArea()));
+  }
+
   /**
    * Minimum Bounding Rectangle of a geometry.
    * Plus Petit Rectangle Englobant d'une géométrie.
@@ -1717,5 +1744,79 @@ public class JtsAlgorithms implements GeomAlgorithms {
           trous[j]=geom.getFactory().createLinearRing(coord2);
       }
       return geom.getFactory().createPolygon(shell, trous);
+  }
+
+  /**
+   * Calcule l'homothétie d'une géométrie.
+   * @param geom géométrie, geometry
+   * @param x0 position en X du centre de l'homothétie, X position of the center of the operation
+   * @param y0 position en Y du centre de l'homothétie, Y position of the center of the operation
+   * @param scale facteur d'échelle, scale factor
+   * @return polygon résultant de l'homothétie, resulting polygon
+   */
+  public static Polygon homothetie(Polygon geom, double x0, double y0, double scale){
+      //le contour externe
+      Coordinate[] coord=geom.getExteriorRing().getCoordinates();
+      Coordinate[] coord_=new Coordinate[coord.length];
+      for(int i=0;i<coord.length;i++) coord_[i]=new Coordinate(x0+scale*(coord[i].x-x0), y0+scale*(coord[i].y-y0));
+      LinearRing lr=geom.getFactory().createLinearRing(coord_);
+
+      //les trous
+      LinearRing[] trous=new LinearRing[geom.getNumInteriorRing()];
+      for(int j=0;j<geom.getNumInteriorRing();j++){
+          Coordinate[] hole_coord=geom.getInteriorRingN(j).getCoordinates();
+          Coordinate[] hole_coord_=new Coordinate[hole_coord.length];
+          for(int i=0;i<hole_coord.length;i++) hole_coord_[i]=new Coordinate(x0+scale*(hole_coord[i].x-x0), y0+scale*(hole_coord[i].y-y0));
+          trous[j]=geom.getFactory().createLinearRing(hole_coord_);
+      }
+      return geom.getFactory().createPolygon(lr,trous);
+  }
+
+     /**
+   * Calcule l'homothétie d'une géométrie.
+   * @param geom géométrie, geometry
+   * @param x0 position en X du centre de l'homothétie, X position of the center of the operation
+   * @param y0 position en Y du centre de l'homothétie, Y position of the center of the operation
+   * @param scaleX facteur d'échelle en X, X scale factor
+   * @param scaleY facteur d'échelle en Y, Y scale factor
+   * @return polygon résultant de l'homothétie, resulting polygon
+   */
+  public static Polygon homothetie(Polygon geom, double x0, double y0, double scaleX, double scaleY){
+      //le contour externe
+      Coordinate[] coord=geom.getExteriorRing().getCoordinates();
+      Coordinate[] coord_=new Coordinate[coord.length];
+      for(int i=0;i<coord.length;i++) coord_[i]=new Coordinate(x0+scaleX*(coord[i].x-x0), y0+scaleY*(coord[i].y-y0));
+      LinearRing lr=geom.getFactory().createLinearRing(coord_);
+
+      //les trous
+      LinearRing[] trous=new LinearRing[geom.getNumInteriorRing()];
+      for(int j=0;j<geom.getNumInteriorRing();j++){
+          Coordinate[] hole_coord=geom.getInteriorRingN(j).getCoordinates();
+          Coordinate[] hole_coord_=new Coordinate[hole_coord.length];
+          for(int i=0;i<hole_coord.length;i++) hole_coord_[i]=new Coordinate(x0+scaleY*(hole_coord[i].x-x0), y0+scaleY*(hole_coord[i].y-y0));
+          trous[j]=geom.getFactory().createLinearRing(hole_coord_);
+      }
+      return geom.getFactory().createPolygon(lr,trous);
+  }
+
+  /**
+   * Calcule l'homothétie d'une géométrie.
+   * @param geom géométrie, geometry
+   * @param scaleX facteur d'échelle en X, X scale factor
+   * @param scaleY facteur d'échelle en Y, Y scale factor
+   * @return polygon résultant de l'homothétie, resulting polygon
+   */
+  public static Polygon homothetie(Polygon geom, double scaleX, double scaleY){
+      return homothetie(geom, geom.getCentroid().getX(), geom.getCentroid().getY(), scaleX, scaleY);
+  }
+
+  /**
+   * Calcule l'homothétie d'une géométrie.
+   * @param geom géométrie, geometry
+   * @param scale facteur d'échelle, scale factor
+   * @return polygon résultant de l'homothétie, resulting polygon
+   */
+  public static Polygon homothetie(Polygon geom, double scale){
+      return homothetie(geom, geom.getCentroid().getX(), geom.getCentroid().getY(), scale);
   }
 } // class
