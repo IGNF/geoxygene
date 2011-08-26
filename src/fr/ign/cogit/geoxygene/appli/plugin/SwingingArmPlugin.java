b1ae6ac09afa26593cad14737bb6e1ec0fc5c8f9
Julien Perret <Julien.perret@gmail.com>
2011-08-26 17:11:26 +0000

    Added plugin for Swinging arm non convex hull computation (and comment to this algorithm).

---
+++ src/fr/ign/cogit/geoxygene/appli/plugin/SwingingArmPlugin.java
@@ -0,0 +1,133 @@
+/*
+ * This file is part of the GeOxygene project source files. GeOxygene aims at
+ * providing an open framework which implements OGC/ISO specifications for the
+ * development and deployment of geographic (GIS) applications. It is a open
+ * source contribution of the COGIT laboratory at the Institut Géographique
+ * National (the French National Mapping Agency). See:
+ * http://oxygene-project.sourceforge.net Copyright (C) 2005 Institut
+ * Géographique National This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the License,
+ * or any later version. This library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details. You should have received a copy of
+ * the GNU Lesser General Public License along with this library (see file
+ * LICENSE if present); if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package fr.ign.cogit.geoxygene.appli.plugin;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import fr.ign.cogit.geoxygene.api.feature.IFeature;
+import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IDirectPosition;
+import fr.ign.cogit.geoxygene.api.spatial.coordgeom.ILineString;
+import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
+import fr.ign.cogit.geoxygene.appli.GeOxygeneApplication;
+import fr.ign.cogit.geoxygene.appli.ProjectFrame;
+import fr.ign.cogit.geoxygene.contrib.algorithms.SwingingArmNonConvexHull;
+import fr.ign.cogit.geoxygene.contrib.cartetopo.Face;
+import fr.ign.cogit.geoxygene.contrib.delaunay.Triangulation;
+import fr.ign.cogit.geoxygene.contrib.geometrie.Operateurs;
+import fr.ign.cogit.geoxygene.feature.DataSet;
+import fr.ign.cogit.geoxygene.feature.Population;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_LineString;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_Polygon;
+import fr.ign.cogit.geoxygene.spatial.geomaggr.GM_MultiCurve;
+import fr.ign.cogit.geoxygene.style.Layer;
+
+/**
+ * Triangulation plugin.
+ * @author Julien Perret
+ */
+public class SwingingArmPlugin implements GeOxygeneApplicationPlugin,
+    ActionListener {
+  /**
+   * Logger.
+   */
+  static Logger logger = Logger.getLogger(Triangulation.class.getName());
+
+  private GeOxygeneApplication application = null;
+
+  /**
+   * Initialize the plugin.
+   * @param application the application
+   */
+  @Override
+public final void initialize(final GeOxygeneApplication application) {
+    this.application = application;
+    JMenu menu = null;
+    for (Component c : application.getFrame().getJMenuBar().getComponents()) {
+      if (c instanceof JMenu) {
+        JMenu aMenu = (JMenu) c;
+        if (aMenu.getText() != null && aMenu.getText().equalsIgnoreCase("Triangulation")) {
+          menu = aMenu;
+        }
+      }
+    }
+    if (menu == null) {
+      menu = new JMenu("Triangulation");//$NON-NLS-1$
+    }
+    JMenuItem menuItem = new JMenuItem("SwingingArmNonConvexHull" //$NON-NLS-1$
+    );
+    menuItem.addActionListener(this);
+    menu.add(menuItem);
+    application.getFrame().getJMenuBar().add(menu,
+        application.getFrame().getJMenuBar().getMenuCount() - 2);
+  }
+
+  @Override
+  public void actionPerformed(final ActionEvent e) {
+    ProjectFrame project = this.application.getFrame()
+        .getSelectedProjectFrame();
+    Set<Layer> selectedLayers = project.getLayerLegendPanel()
+        .getSelectedLayers();
+    if (selectedLayers.size() != 1) {
+      SwingingArmPlugin.logger
+          .error("You need to select one (and only one) layer."); //$NON-NLS-1$
+      return;
+    }
+    Layer layer = selectedLayers.iterator().next();
+    double radius = Double.parseDouble(JOptionPane.showInputDialog("radius")); //$NON-NLS-1$
+    List<IDirectPosition> list = new ArrayList<IDirectPosition>();
+    for (IFeature f : layer.getFeatureCollection()) {
+      GM_MultiCurve multi = (GM_MultiCurve) f.getGeom();
+      if (multi.size() > 0) {
+        ILineString line = (GM_LineString) multi.get(0);
+        line = Operateurs.echantillonePasVariable(line, radius / 2);
+        list.addAll(line.coord().getList());
+      }
+    }
+    logger.error("Points added : " + list.size());
+    SwingingArmNonConvexHull.logger.setLevel(Level.TRACE);
+    SwingingArmNonConvexHull swinging = new SwingingArmNonConvexHull(list, radius);
+    IGeometry characteristicShape = swinging.compute();
+//    logger.error("Shape : " + characteristicShape.getClass());
+    Population<Face> popTriangles = new Population<Face>("NonConvexHull_"+radius); //$NON-NLS-1$
+    popTriangles.setClasse(Face.class);
+    popTriangles.setPersistant(false);
+    popTriangles.nouvelElement(characteristicShape);
+    /** créer un featuretype de jeu correspondant */
+    fr.ign.cogit.geoxygene.schema.schemaConceptuelISOJeu.FeatureType newFeatureTypeExterieurs = new fr.ign.cogit.geoxygene.schema.schemaConceptuelISOJeu.FeatureType();
+    newFeatureTypeExterieurs.setGeometryType(GM_Polygon.class);
+    popTriangles.setFeatureType(newFeatureTypeExterieurs);
+    DataSet.getInstance().addPopulation(popTriangles);
+    logger.info(popTriangles.size());
+    logger.info(characteristicShape);
+    project.addFeatureCollection(popTriangles, popTriangles.getNom());
+  }
+}
--- src/fr/ign/cogit/geoxygene/contrib/algorithms/SwingingArmNonConvexHull.java
+++ src/fr/ign/cogit/geoxygene/contrib/algorithms/SwingingArmNonConvexHull.java
@@ -1,55 +1,51 @@
 package fr.ign.cogit.geoxygene.contrib.algorithms;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
 import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IDirectPosition;
-import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IDirectPositionList;
-import fr.ign.cogit.geoxygene.api.spatial.coordgeom.ILineString;
 import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPosition;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPositionList;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_LineString;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_Polygon;
 import fr.ign.cogit.geoxygene.spatial.geomprim.GM_Point;
+import fr.ign.cogit.geoxygene.spatial.geomroot.GM_Object;
 
 /**
- * Computation of a non convex hull using swinging arm method
- * @author J. Renard (d'apr�s M. Duckham) 11/08/2009
- * 
+ * Computation of a non convex hull using swinging arm method.
+ * <p>
+ * Galton, A., Duckham, M. (2006) What is the Region Occupied by a Set of
+ * Points? In Raubal, M., Miller, H., Frank, A., and Goodchild, M. (eds) Lecture
+ * Notes in Computer Science 4197. Springer, Berlin.
+ * @author J. Renard (d'après Galton & Duckham) 11/08/2009
  */
 public class SwingingArmNonConvexHull {
-  private final static Logger logger = Logger
+  public final static Logger logger = Logger
       .getLogger(SwingingArmNonConvexHull.class.getName());
-
   /**
    * Points to construct the hull
    */
-  private ArrayList<IDirectPosition> points;
-
+  private List<IDirectPosition> points;
   /**
    * Swinging arm radius
    */
   private double radius = 0.0;
-
   /**
    * Constructor
    */
-  public SwingingArmNonConvexHull(ArrayList<IDirectPosition> points,
-      double radius) {
-    this.points = points;
+  public SwingingArmNonConvexHull(List<IDirectPosition> list, double radius) {
+    this.points = list;
     this.radius = radius;
   }
-
   /**
    * Method launch The idea is to compute two non-convex hulls using swinging
    * arm algorithm, one clockwise and one anticlockwise, and then to return the
    * union of these two non-convex hulls
    */
-
   public IGeometry compute() {
-
     if (this.points.isEmpty()) {
       if (SwingingArmNonConvexHull.logger.isTraceEnabled()) {
         SwingingArmNonConvexHull.logger
@@ -57,25 +53,21 @@ public class SwingingArmNonConvexHull {
       }
       return null;
     }
-
     if (SwingingArmNonConvexHull.logger.isTraceEnabled()) {
       SwingingArmNonConvexHull.logger
           .trace("Computation of the non-convex hull using Swinging Arm method with radius="
               + this.radius);
     }
-
     // Swinging arm clockwise
     if (SwingingArmNonConvexHull.logger.isTraceEnabled()) {
       SwingingArmNonConvexHull.logger.trace("clockwise");
     }
     IGeometry clockWise = this.swingingArmCompute(-1.0);
-
     // Swinging arm anticlockwise
     if (SwingingArmNonConvexHull.logger.isTraceEnabled()) {
       SwingingArmNonConvexHull.logger.trace("anticlockwise");
     }
     IGeometry antiClockWise = this.swingingArmCompute(1.0);
-
     // Union of the two geometries
     if (SwingingArmNonConvexHull.logger.isTraceEnabled()) {
       SwingingArmNonConvexHull.logger.trace("union of the two hulls");
@@ -88,7 +80,6 @@ public class SwingingArmNonConvexHull {
       return clockWise;
     }
     return clockWise.union(antiClockWise);
-
   }
 
   /**
@@ -99,9 +90,8 @@ public class SwingingArmNonConvexHull {
    * point is recovered, it forms a connex component, and the union of all
    * connex components is the non-convex hull
    */
-
   private IGeometry swingingArmCompute(double direction) {
-
+    // if there is no point, return a null hull
     if (this.points.isEmpty()) {
       if (SwingingArmNonConvexHull.logger.isTraceEnabled()) {
         SwingingArmNonConvexHull.logger
@@ -109,74 +99,71 @@ public class SwingingArmNonConvexHull {
       }
       return null;
     }
-
     IGeometry hull = new GM_Polygon(new GM_LineString(new DirectPositionList()));
     int nbConnexComponents = 0;
-
+    /*
+     * Mark all points of S as ‘available’ and ‘unvisited’.
+     */
     // Boolean list for available points
-    ArrayList<String> isAvailable = new ArrayList<String>(this.points.size());
+    List<Boolean> isAvailable = new ArrayList<Boolean>(this.points.size());
     for (int i = 0; i < this.points.size(); i++) {
-      isAvailable.add("true");
+      isAvailable.add(Boolean.TRUE);
     }
-
     // Search for points to construct connex components util they are all
     // unavailable
-    while (isAvailable.contains("true")) {
-
-      IDirectPositionList hullPoints = new DirectPositionList();
-
+    while (isAvailable.contains(Boolean.TRUE)) {
+      DirectPositionList hullPoints = new DirectPositionList();
       // List of absolute positions of points of the current connex component
       ArrayList<Integer> absPos = new ArrayList<Integer>();
-
       // Line being constructed (to verify non self intersection)
-      ILineString provisoryLine = new GM_LineString();
-
+      GM_LineString provisoryLine = new GM_LineString();
       // Boolean list for visited points
-      ArrayList<String> isVisited = new ArrayList<String>(this.points.size());
+      List<Boolean> isVisited = new ArrayList<Boolean>(this.points.size());
       for (int i = 0; i < this.points.size(); i++) {
-        isVisited.add("false");
+        isVisited.add(Boolean.FALSE);
       }
-
       // Initial point of the current connex component
-
       IDirectPosition initialPoint = new DirectPosition();
-      double Xmax = 0.0;
-      double Ymax = 0.0;
-      int ptPos = 0;
-
+      double Xmax = Double.NEGATIVE_INFINITY;
+      double Ymax = Double.NEGATIVE_INFINITY;
+      int ptPos = -1;
+      /*
+       * Let Hi,0 be the available point in S with maximal y-coordinate (if
+       * there is more than one such, choose the one with minimal x-coordinate).
+       */
       for (int pos = 0; pos < this.points.size(); pos++) {
-        if (isAvailable.get(pos) == "false") {
+        if (isAvailable.get(pos).equals(Boolean.FALSE)) {
           continue;
         }
         if ((this.points.get(pos).getY() > Ymax)) {
-          Ymax = this.points.get(pos).getY();
-          Xmax = this.points.get(pos).getX();
           initialPoint = this.points.get(pos);
+          Ymax = initialPoint.getY();
+          Xmax = initialPoint.getX();
           ptPos = pos;
         } else if ((this.points.get(pos).getY() == Ymax)
             && (this.points.get(pos).getX() > Xmax)) {
-          Xmax = this.points.get(pos).getX();
           initialPoint = this.points.get(pos);
+          Xmax = initialPoint.getX();
           ptPos = pos;
         }
       }
-
       // Addition of the initial point to the hull
       hullPoints.add(initialPoint);
+      /*
+       * Let L be a line-segment of length r anchored at Hi,0 parallel to the
+       * positive y-axis.
+       */
       provisoryLine.addControlPoint(initialPoint);
       absPos.add(new Integer(ptPos));
-
       // Following points of the current connex component
       while (true) {
-
         // Initialisation of parameters
         IDirectPosition nextPoint = new DirectPosition();
-        IDirectPosition lastPoint = this.points.get(absPos.get(
-            absPos.size() - 1).intValue());
-        double alphaMin = Double.MAX_VALUE;
+        IDirectPosition lastPoint = this.points.get(absPos
+            .get(absPos.size() - 1).intValue());
+        double alphaMin = Double.POSITIVE_INFINITY;
         double ptRadius = this.radius;
         ptPos = 0;
-
         // Angle of the last segment
         double alphaDiff = Math.PI / 2.0;
         if (absPos.size() > 1) {
@@ -191,17 +178,15 @@ public class SwingingArmNonConvexHull {
             alphaDiff += 2 * Math.PI;
           }
         }
-
         // Searching for the next point
         for (int pos = 0; pos < this.points.size(); pos++) {
           IDirectPosition pt = this.points.get(pos);
-
           // Point not considered if: not available, already visited, too far
           // from last point, or same as last two points
-          if (isAvailable.get(pos) == "false") {
+          if (isAvailable.get(pos).equals(Boolean.FALSE)) {
             continue;
           }
-          if (isVisited.get(pos) == "true") {
+          if (isVisited.get(pos).equals(Boolean.TRUE)) {
             continue;
           }
           if (pt.distance(lastPoint) > this.radius) {
@@ -217,11 +202,10 @@ public class SwingingArmNonConvexHull {
               continue;
             }
           }
-
           // Particular case: the potential next segment intersects the line
           // being constructed
           if (absPos.size() > 2 && !absPos.contains(new Integer(pos))) {
-            ILineString potentialSegment = new GM_LineString();
+            GM_LineString potentialSegment = new GM_LineString();
             potentialSegment.addControlPoint(lastPoint);
             potentialSegment.addControlPoint(pt);
             provisoryLine.removeControlPoint(absPos.size() - 1);
@@ -231,7 +215,6 @@ public class SwingingArmNonConvexHull {
             }
             provisoryLine.addControlPoint(lastPoint);
           }
-
           // Computation of the angle
           double alpha = Math.atan((pt.getY() - lastPoint.getY())
               / (pt.getX() - lastPoint.getX()));
@@ -246,7 +229,6 @@ public class SwingingArmNonConvexHull {
           if (alpha < 0) {
             alpha += 2 * Math.PI;
           }
-
           // Point kept as next one if the angle to searching direction is
           // minimum
           if (alpha < alphaMin
@@ -256,13 +238,12 @@ public class SwingingArmNonConvexHull {
             nextPoint = pt;
             ptPos = pos;
           }
-
         }
-
         // If no following point is found, the current connex component is
         // unvalid
-        if (alphaMin == Double.MAX_VALUE) {
-          isAvailable.set(absPos.get(absPos.size() - 1).intValue(), "false");
+        if (Double.isInfinite(alphaMin)) {
+          isAvailable.set(absPos.get(absPos.size() - 1).intValue(),
+              Boolean.FALSE);
           // If the connex component is reduces to one point, it is unvalid
           if (absPos.size() == 1) {
             break;
@@ -273,65 +254,57 @@ public class SwingingArmNonConvexHull {
           provisoryLine
               .removeControlPoint(provisoryLine.sizeControlPoint() - 1);
           absPos.remove(absPos.size() - 1);
-
           continue;
         }
-
         // Addition of the next point to the current connex component
         hullPoints.add(nextPoint);
         provisoryLine.addControlPoint(nextPoint);
         absPos.add(new Integer(ptPos));
-        isVisited.set(ptPos, "true");
-
+        isVisited.set(ptPos, Boolean.TRUE);
         // If the next point the initial point of the current connex component,
         // it is closed
         // the connex component has to be added to the hull and all interior
         // points are marked unavailable
         if (nextPoint.equals(initialPoint)) {
-
           // Addition of all points of the connex component to unavailable
           // points
           for (Integer k : absPos) {
-            if (isAvailable.get(k.intValue()) == "true") {
-              isAvailable.set(k.intValue(), "false");
+            if (isAvailable.get(k.intValue()).equals(Boolean.TRUE)) {
+              isAvailable.set(k.intValue(), Boolean.FALSE);
             }
           }
-
           // Addition of the component connex
-          IGeometry connexComponent = new GM_Polygon(new GM_LineString(
+          GM_Object connexComponent = new GM_Polygon(new GM_LineString(
               hullPoints));
-          if (hull == null || hull.isEmpty()) {
+          if (hull == null || hull.isEmpty() || !hull.isValid()) {
             hull = connexComponent;
           } else {
-            if (!(hull.union(connexComponent) == null)) {
-              hull = hull.union(connexComponent);
+            IGeometry union = hull.union(connexComponent);
+            if (union != null) {
+              hull = union;
             }
           }
           if (!connexComponent.isValid()) {
-            System.out
-                .println("non valid connex component: " + connexComponent);
+            logger.error("non valid connex component: " + connexComponent);
           }
           nbConnexComponents++;
-
           // Addition of all interior points to unavailable points
           for (int k = 0; k < this.points.size(); k++) {
-            if (isAvailable.get(k) == "false") {
+            if (isAvailable.get(k).equals(Boolean.FALSE)) {
               continue;
             }
             if (connexComponent.contains(new GM_Point(this.points.get(k)))) {
-              isAvailable.set(k, "false");
+              isAvailable.set(k, Boolean.FALSE);
             }
           }
           break;
         }
       }
     }
-
     if (SwingingArmNonConvexHull.logger.isTraceEnabled()) {
       SwingingArmNonConvexHull.logger.trace("non-convex-hull containing "
           + nbConnexComponents + " connex components");
     }
     return hull;
   }
-
 }
