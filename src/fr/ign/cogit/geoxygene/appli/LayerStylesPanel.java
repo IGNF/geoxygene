04ba706d66e33bb4182072084525688033c2b79d
Julien Perret <Julien.perret@gmail.com>
2012-02-06 13:01:07 +0000

    Using filters instead of rules to create style panels.

--- src/fr/ign/cogit/geoxygene/appli/LayerStylesPanel.java
+++ src/fr/ign/cogit/geoxygene/appli/LayerStylesPanel.java
@@ -29,11 +29,18 @@ import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.media.jai.PlanarImage;
 import javax.swing.JPanel;
+
 import fr.ign.cogit.geoxygene.api.feature.IFeature;
 import fr.ign.cogit.geoxygene.feature.FT_Coverage;
+import fr.ign.cogit.geoxygene.filter.Filter;
 import fr.ign.cogit.geoxygene.style.ExternalGraphic;
 import fr.ign.cogit.geoxygene.style.FeatureTypeStyle;
 import fr.ign.cogit.geoxygene.style.Layer;
@@ -45,7 +52,6 @@ import fr.ign.cogit.geoxygene.style.RasterSymbolizer;
 import fr.ign.cogit.geoxygene.style.Rule;
 import fr.ign.cogit.geoxygene.style.Style;
 import fr.ign.cogit.geoxygene.style.Symbolizer;
-import fr.ign.cogit.geoxygene.style.UserStyle;
 
 /**
  * @author Julien Perret
@@ -53,17 +59,23 @@ import fr.ign.cogit.geoxygene.style.UserStyle;
 public class LayerStylesPanel extends JPanel {
 
   /**
-   * serial uid.
+   * Serial uid.
    */
   private static final long serialVersionUID = 1L;
+  /**
+   * The layer the panel corresponds to.
+   */
   private WeakReference<Layer> layer;
-
+  /**
+   * @return the layer the panel corresponds to
+   */
   public Layer getLayer() {
     return this.layer.get();
   }
-
+  /**
+   * A margin around the panel.
+   */
   private int margin = 2;
-
   /**
    * Constructor.
    */
@@ -71,96 +83,102 @@ public class LayerStylesPanel extends JPanel {
     this.layer = new WeakReference<Layer>(aLayer);
     this.setBackground(Color.white);
   }
-
   @Override
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D) g;
+    // store the initial color of the graphics
     Color originalColor = g.getColor();
+    // clear the graphics using the background color
     g.setColor(this.getBackground());
     g2.setStroke(new BasicStroke());
     g.fillRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
-    // count how many rules we have
-    int numberOfRules = 0;
+    // count the number of filters
+    Map<Filter, List<Symbolizer>> map = new HashMap<Filter, List<Symbolizer>>();
     for (Style style : this.layer.get().getStyles()) {
-      UserStyle userStyle = (UserStyle) style;
-      for (FeatureTypeStyle fts : userStyle.getFeatureTypeStyles()) {
-        numberOfRules += fts.getRules().size();
-      }
-    }
-    int numberOfColumns = Math.round((float) Math.sqrt(numberOfRules));
-    int numberOfRows = numberOfRules / numberOfColumns;
-
-    int columnsWidth = (this.getWidth() - numberOfColumns * this.margin - this.margin)
-        / numberOfColumns;
-    int rowHeight = (this.getHeight() - numberOfRows * this.margin - this.margin)
-        / numberOfRows;
-
-    int currentColumn = 0;
-    int currentRow = 0;
-
-    for (Style style : this.layer.get().getStyles()) {
-      UserStyle userStyle = (UserStyle) style;
-      for (FeatureTypeStyle fts : userStyle.getFeatureTypeStyles()) {
-        for (Rule rule : fts.getRules()) {
-          for (Symbolizer symbolizer : rule.getSymbolizers()) {
-            // g.setColor(symbolizer.getStroke().getColor());
-            if (symbolizer.isLineSymbolizer()) {
-              this.paintLine((LineSymbolizer) symbolizer, g2, currentColumn,
-                  currentRow, columnsWidth, rowHeight);
-            } else {
-              if (symbolizer.isPointSymbolizer()) {
-                this.paintPoint((PointSymbolizer) symbolizer, g2,
-                    currentColumn, currentRow, columnsWidth, rowHeight);
-                currentRow++;
-              } else {
-                if (symbolizer.isPolygonSymbolizer()) {
-                  this.paintPolygon((PolygonSymbolizer) symbolizer, g2,
-                      currentColumn, currentRow, columnsWidth, rowHeight);
-                  currentRow++;
-                } else {
-                  if (symbolizer.isRasterSymbolizer()) {
-                    this.paintRaster((RasterSymbolizer) symbolizer, g2,
-                        currentColumn, currentRow, columnsWidth, rowHeight);
-                    currentRow++;
-                  }
-                }
-              }
-            }
-          }
-          currentColumn++;
-          if (currentColumn >= numberOfColumns) {
-            currentColumn = 0;
+      for (FeatureTypeStyle fts : style.getFeatureTypeStyles()) {
+        for (Rule r : fts.getRules()) {
+          if (!map.containsKey(r.getFilter())) {
+            map.put(r.getFilter(), new ArrayList<Symbolizer>(r.getSymbolizers()));
+          } else {
+            map.get(r.getFilter()).addAll(r.getSymbolizers());
           }
         }
       }
     }
+    int numberOfColumns = Math.round((float) Math.sqrt(map.keySet().size()));
+    int numberOfRows = map.keySet().size() / numberOfColumns;
+    int columnsWidth = (this.getWidth() - numberOfColumns * this.margin - this.margin)
+        / numberOfColumns;
+    int rowHeight = (this.getHeight() - numberOfRows * this.margin - this.margin)
+        / numberOfRows;
+    int currentColumn = 0;
+    int currentRow = 0;
+    for (Entry<Filter, List<Symbolizer>> entry : map.entrySet()) {
+      List<Symbolizer> symbolizers = entry.getValue();
+      for (Symbolizer symbolizer : symbolizers) {
+        if (symbolizer.isLineSymbolizer()) {
+          this.paintLine((LineSymbolizer) symbolizer, g2, currentColumn,
+              currentRow, columnsWidth, rowHeight);
+        } else {
+          if (symbolizer.isPointSymbolizer()) {
+            this.paintPoint((PointSymbolizer) symbolizer, g2,
+                currentColumn, currentRow, columnsWidth, rowHeight);
+          } else {
+            if (symbolizer.isPolygonSymbolizer()) {
+              this.paintPolygon((PolygonSymbolizer) symbolizer, g2,
+                  currentColumn, currentRow, columnsWidth, rowHeight);
+            } else {
+              if (symbolizer.isRasterSymbolizer()) {
+                this.paintRaster((RasterSymbolizer) symbolizer, g2,
+                    currentColumn, currentRow, columnsWidth, rowHeight);
+              }
+            }
+          }
+        }
+      }
+      currentColumn++;
+      if (currentColumn >= numberOfColumns) {
+        currentColumn = 0;
+        currentRow++;
+      }
+    }
+    // restore the initial color
     g.setColor(originalColor);
   }
-
+  /**
+   * Paint a line symbolizer at the given position.
+   * @param symbolizer the symbolizer
+   * @param g2 the graphics to paint in
+   * @param currentColumn
+   * @param currentRow
+   * @param columnsWidth
+   * @param rowHeight
+   */
   private void paintLine(LineSymbolizer symbolizer, Graphics2D g2,
       int currentColumn, int currentRow, int columnsWidth, int rowHeight) {
-    g2.setStroke(symbolizer.getStroke().toAwtStroke());
-    g2.setColor(symbolizer.getStroke().getColor());
-    if (this.layer.get().getStyles().size() == 1) {
-      g2.drawLine(currentColumn * (columnsWidth + this.margin), currentRow
-          * (rowHeight + this.margin) + rowHeight / 2, (currentColumn + 1)
-          * (columnsWidth + this.margin) + this.margin, currentRow
-          * (rowHeight + this.margin) + rowHeight / 2);
-    } else {
-      g2.drawLine(currentColumn * (columnsWidth + this.margin), currentRow
-          * (rowHeight + this.margin) + rowHeight, (currentColumn + 1)
-          * (columnsWidth + this.margin) + this.margin, currentRow
-          * (rowHeight + this.margin) + rowHeight);
+    if (symbolizer.getStroke() != null) {
+      g2.setStroke(symbolizer.getStroke().toAwtStroke());
+      g2.setColor(symbolizer.getStroke().getColor());
     }
+    g2.drawLine(currentColumn * (columnsWidth + this.margin), currentRow
+        * (rowHeight + this.margin) + rowHeight / 2, (currentColumn + 1)
+        * (columnsWidth + this.margin) + this.margin, currentRow
+        * (rowHeight + this.margin) + rowHeight / 2);
   }
-
   @Override
   public void setBounds(int x, int y, int w, int h) {
     super.setBounds(x, y, w, h);
     this.validate();
   }
-
+  /**
+   * @param symbolizer
+   * @param g2
+   * @param currentColumn
+   * @param currentRow
+   * @param columnsWidth
+   * @param rowHeight
+   */
   private void paintPoint(PointSymbolizer symbolizer, Graphics2D g2,
       int currentColumn, int currentRow, int columnsWidth, int rowHeight) {
     for (Mark mark : symbolizer.getGraphic().getMarks()) {
@@ -192,7 +210,14 @@ public class LayerStylesPanel extends JPanel {
               - onlineImage.getHeight(null) / 2, null);
     }
   }
-
+  /**
+   * @param symbolizer
+   * @param g2
+   * @param currentColumn
+   * @param currentRow
+   * @param columnsWidth
+   * @param rowHeight
+   */
   private void paintPolygon(PolygonSymbolizer symbolizer, Graphics2D g2,
       int currentColumn, int currentRow, int columnsWidth, int rowHeight) {
     if (symbolizer.getFill() != null) {
@@ -209,20 +234,25 @@ public class LayerStylesPanel extends JPanel {
           rowHeight);
     }
   }
-
+  /**
+   * @param symbolizer
+   * @param g2
+   * @param currentColumn
+   * @param currentRow
+   * @param columnsWidth
+   * @param rowHeight
+   */
   private void paintRaster(RasterSymbolizer symbolizer, Graphics2D g2,
       int currentColumn, int currentRow, int columnsWidth, int rowHeight) {
     int x = currentColumn * (columnsWidth + this.margin) + this.margin;
     int y = currentRow * (rowHeight + this.margin) + this.margin;
     int width = columnsWidth;
     int height = rowHeight;
-    
     //FIXME Pour l'instant on considère qu'il n'y a qu'un symbolizer
     for(IFeature feature : this.layer.get().getFeatureCollection()){
         FT_Coverage coverage = (FT_Coverage) feature;
         BufferedImage image =  PlanarImage.wrapRenderedImage(coverage.coverage().getRenderedImage()).getAsBufferedImage();
         g2.drawImage(image, x, y,width,height, null);
-    }
-    
+    } 
   }
 }
