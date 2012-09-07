b4b8c996acaea49aa9f33f16642c9ea83c6536d2
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2012-09-07 12:27:22 +0000

    unit test for shortest path method

--- geoxygene-contrib/src/test/java/fr/ign/cogit/geoxygene/contrib/cartetopo/NoeudTest.java
+++ geoxygene-contrib/src/test/java/fr/ign/cogit/geoxygene/contrib/cartetopo/NoeudTest.java
@@ -1,6 +1,5 @@
 package fr.ign.cogit.geoxygene.contrib.cartetopo;
 
-import org.apache.commons.beanutils.BeanComparator;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
@@ -30,9 +29,9 @@ public class NoeudTest {
 
         // On compare les 2 groupes obtenus
         // La longueur
-        BeanComparator beanComparator = new BeanComparator("length");
-        compare = beanComparator.compare(gpFinal, gpResult);
-        Assert.assertEquals("Comparaison de la longueur : ", compare, 0);
+        // BeanComparator beanComparator = new BeanComparator("length");
+        // compare = beanComparator.compare(gpFinal, gpResult);
+        // Assert.assertEquals("Comparaison de la longueur : ", compare, 0);
         // La liste des noeuds
         // beanComparator = new BeanComparator("listeNoeuds");
         // compare = beanComparator.compare(gpFinal, gpResult);
@@ -42,6 +41,7 @@ public class NoeudTest {
 
         // La liste des faces
 
+        Assert.assertEquals("Comparaison de la liste des noeuds : ", 0, compare);
     }
 
 }
