cc40fac54059184a6d7a6e58d15b201149d42a3e
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2013-05-06 16:22:17 +0000

    add filefilter for xmlfile and shapefile

---
+++ geoxygene-io/src/main/java/fr/ign/cogit/geoxygene/filter/ShapeFilter.java
@@ -0,0 +1,22 @@
+package fr.ign.cogit.geoxygene.filter;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ *
+ */
+public class ShapeFilter implements FilenameFilter {
+
+    public boolean accept(File dir, String name) {
+        return (name.endsWith(".shp"));
+    }
+    
+    public boolean accept(final File f) {
+      return (f.isFile() && (f.getAbsolutePath().endsWith(".shp") || f.getAbsolutePath().endsWith(".SHP")) || f.isDirectory());
+    }
+
+    public String getDescription() {
+        return "ShapefileReader.ESRIShapefiles";
+    }
+}
---
+++ geoxygene-io/src/main/java/fr/ign/cogit/geoxygene/filter/XMLFilter.java
@@ -0,0 +1,21 @@
+package fr.ign.cogit.geoxygene.filter;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class XMLFilter implements FilenameFilter {
+  
+  public boolean accept(File dir, String name) {
+      return (name.endsWith(".xml"));
+  }
+  
+  public boolean accept(final File f) {
+    return (f.isFile() && (f.getAbsolutePath().endsWith(".xml") //$NON-NLS-1$
+        || f.getAbsolutePath().endsWith(".XML") //$NON-NLS-1$
+        ) || f.isDirectory());
+  }
+
+  public String getDescription() {
+    return "XML Files"; //$NON-NLS-1$
+  }
+}
