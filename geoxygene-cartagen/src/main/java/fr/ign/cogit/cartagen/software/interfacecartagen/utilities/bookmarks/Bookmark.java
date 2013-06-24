1732721f57af1ebfa689047ac517721bb777690e
Jeremy Renard <Jeremy.Renard@ign.fr>
2013-06-24 12:16:37 +0000

    bookmark constructor in public

--- geoxygene-cartagen/src/main/java/fr/ign/cogit/cartagen/software/interfacecartagen/utilities/bookmarks/Bookmark.java
+++ geoxygene-cartagen/src/main/java/fr/ign/cogit/cartagen/software/interfacecartagen/utilities/bookmarks/Bookmark.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * This software is released under the licence CeCILL
- *  
- *  see Licence_CeCILL-C_fr.html see Licence_CeCILL-C_en.html
- *  
- *  see <a href="http://www.cecill.info/">http://www.cecill.info/a>
- *  
- *  @copyright IGN
+ * 
+ * see Licence_CeCILL-C_fr.html see Licence_CeCILL-C_en.html
+ * 
+ * see <a href="http://www.cecill.info/">http://www.cecill.info/a>
+ * 
+ * @copyright IGN
  ******************************************************************************/
 package fr.ign.cogit.cartagen.software.interfacecartagen.utilities.bookmarks;
 
 import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IEnvelope;
 
 /**
- * A bookmark stores how the CartAGen window is centred and zoomed on a 
+ * A bookmark stores how the CartAGen window is centred and zoomed on a
  * particular place of a Cartagen dataset.
  * 
  */
@@ -20,36 +20,44 @@ public class Bookmark {
   private String datasetName;
   private IEnvelope extent;
   private String name;
-  
+
   /**
    * Constructor based on the three fields of the class
    * @param dataset
    * @param window
    * @param name
    */
-  Bookmark(String dataset,IEnvelope window,String name){
-      this.datasetName = dataset;
-      this.extent = window;
-      this.name = name;
+  public Bookmark(String dataset, IEnvelope window, String name) {
+    this.datasetName = dataset;
+    this.extent = window;
+    this.name = name;
   }
-  
+
   @Override
   public boolean equals(Object obj) {
-    if(!(obj instanceof Bookmark)) return false;
+    if (!(obj instanceof Bookmark)) {
+      return false;
+    }
     Bookmark other = (Bookmark) obj;
-    if(!this.name.equals(other.name)) return false;
-    if(!this.datasetName.equals(other.datasetName)) return false;
-    if(!this.extent.equals(other.extent)) return false;
+    if (!this.name.equals(other.name)) {
+      return false;
+    }
+    if (!this.datasetName.equals(other.datasetName)) {
+      return false;
+    }
+    if (!this.extent.equals(other.extent)) {
+      return false;
+    }
     return true;
   }
 
   @Override
-  public String toString(){
-      return this.name;
+  public String toString() {
+    return this.name;
   }
 
   public String getDatasetName() {
-    return datasetName;
+    return this.datasetName;
   }
 
   public void setDatasetName(String dataset) {
@@ -57,7 +65,7 @@ public class Bookmark {
   }
 
   public IEnvelope getExtent() {
-    return extent;
+    return this.extent;
   }
 
   public void setExtent(IEnvelope extent) {
@@ -65,34 +73,42 @@ public class Bookmark {
   }
 
   public String getName() {
-    return name;
+    return this.name;
   }
 
   public void setName(String name) {
     this.name = name;
   }
-  
+
   /**
    * Get the low x coordinate of the extent of the bookmark.
    * @return
    */
-  public double xLo(){return extent.getLowerCorner().getX();}
-  
+  public double xLo() {
+    return this.extent.getLowerCorner().getX();
+  }
+
   /**
    * Get the high x coordinate of the extent of the bookmark.
    * @return
    */
-  public double xHi(){return extent.getUpperCorner().getX();}
-  
+  public double xHi() {
+    return this.extent.getUpperCorner().getX();
+  }
+
   /**
    * Get the low y coordinate of the extent of the bookmark.
    * @return
    */
-  public double yLo(){return extent.getLowerCorner().getY();}
-  
+  public double yLo() {
+    return this.extent.getLowerCorner().getY();
+  }
+
   /**
    * Get the high y coordinate of the extent of the bookmark.
    * @return
    */
-  public double yHi(){return extent.getUpperCorner().getY();}
+  public double yHi() {
+    return this.extent.getUpperCorner().getY();
+  }
 }
