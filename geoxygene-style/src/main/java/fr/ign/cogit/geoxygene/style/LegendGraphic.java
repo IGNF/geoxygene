cc5a8b782d6d28aa59a7b908d6d3957a231700e7
Charlotte Hoarau <charlotte.hoarau@gmail.com>
2013-11-07 16:55:40 +0000

    Adding javadoc comments.

--- geoxygene-style/src/main/java/fr/ign/cogit/geoxygene/style/LegendGraphic.java
+++ geoxygene-style/src/main/java/fr/ign/cogit/geoxygene/style/LegendGraphic.java
@@ -4,6 +4,16 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
+/**
+ * LegendGraphic allows to describe a graphic symbol to be displayed by the
+ * {@link LegendPaintListener} in the legend for each {@link Rule}, by a
+ * {@link Graphic}.
+ * 
+ * @author Julien Perret
+ * 
+ * @see Graphic
+ * @see Rule
+ */
 @XmlAccessorType(XmlAccessType.FIELD)
 public class LegendGraphic {
   @XmlElement(name = "Graphic")
