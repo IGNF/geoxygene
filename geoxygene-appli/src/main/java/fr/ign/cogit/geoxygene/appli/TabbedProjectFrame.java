b05e16aa0143753da4433c6b94be17d4b21f418f
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2014-01-08 15:51:41 +0000

    deplacement de geometryToolBar depuis le package plugin vers le package mode

--- geoxygene-appli/src/main/java/fr/ign/cogit/geoxygene/appli/TabbedProjectFrame.java
+++ geoxygene-appli/src/main/java/fr/ign/cogit/geoxygene/appli/TabbedProjectFrame.java
@@ -30,7 +30,7 @@ import javax.swing.JPanel;
 
 import fr.ign.cogit.geoxygene.api.feature.IPopulation;
 import fr.ign.cogit.geoxygene.appli.layer.LayerViewPanel;
-import fr.ign.cogit.geoxygene.appli.plugin.GeometryToolBar;
+import fr.ign.cogit.geoxygene.appli.mode.GeometryToolBar;
 import fr.ign.cogit.geoxygene.style.Layer;
 import fr.ign.cogit.geoxygene.util.conversion.ShapefileReader;
 
