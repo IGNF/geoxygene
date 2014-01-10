02c04b91ff520f560fad7d9b45f32140b7495fef
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2014-01-10 10:01:07 +0000

    Ménage dans l'appariement réseaux multi-échelle

--- geoxygene-contrib/src/main/java/fr/ign/cogit/geoxygene/contrib/appariement/reseaux/process/Comparaison.java
+++ geoxygene-contrib/src/main/java/fr/ign/cogit/geoxygene/contrib/appariement/reseaux/process/Comparaison.java
@@ -17,7 +17,7 @@
  * Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-package fr.ign.cogit.geoxygene.contrib.appariement.reseaux;
+package fr.ign.cogit.geoxygene.contrib.appariement.reseaux.process;
 
 import java.util.ArrayList;
 import java.util.Collection;
