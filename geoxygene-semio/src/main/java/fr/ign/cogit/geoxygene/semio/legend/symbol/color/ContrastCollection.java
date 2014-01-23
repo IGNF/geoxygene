f27e12b9ad91e93f80e698da2485f79180055bfa
Charlotte Hoarau <charlotte.hoarau@gmail.com>
2014-01-23 16:53:34 +0000

    Correction of the resource stream access method.

--- geoxygene-semio/src/main/java/fr/ign/cogit/geoxygene/semio/legend/symbol/color/ContrastCollection.java
+++ geoxygene-semio/src/main/java/fr/ign/cogit/geoxygene/semio/legend/symbol/color/ContrastCollection.java
@@ -26,196 +26,187 @@ import fr.ign.cogit.geoxygene.style.colorimetry.ColorimetricColor;
 
 /**
  * 
- *        This software is released under the licence CeCILL
+ * This software is released under the licence CeCILL
  * 
- *        see Licence_CeCILL-C_fr.html
- *        see Licence_CeCILL-C_en.html
+ * see Licence_CeCILL-C_fr.html see Licence_CeCILL-C_en.html
  * 
- *        see <a href="http://www.cecill.info/">http://www.cecill.info/a>
+ * see <a href="http://www.cecill.info/">http://www.cecill.info/a>
  * 
  * 
  * @copyright IGN
  * 
  * @author Charlotte Hoarau - IGN / Laboratoire COGIT
- *
+ * 
  * @see Contrast
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "",
-		propOrder = {
-			"name",
-			"nbContrasts",
-			"contrasts"
-		})
+@XmlType(name = "", propOrder = { "name", "nbContrasts", "contrasts" })
 @XmlRootElement(name = "ContrastCollection")
 public class ContrastCollection {
-	
-	static Logger logger = Logger.getLogger(ContrastCollection.class.getName());
-	
-	@XmlElement(name = "Name")
-	protected String name;
-	
-	public String getName() {
-		return this.name;
-	}
 
-	@XmlElement(name = "Contrast")
-	private List<Contrast> contrasts = new ArrayList<Contrast>();
-	
-	public List<Contrast> getContrasts() {
-		return this.contrasts;
-	}
+  static Logger logger = Logger.getLogger(ContrastCollection.class.getName());
 
-	public void setContrasts(List<Contrast> contrasts) {
-		this.contrasts = contrasts;
-		this.nbContrasts = contrasts.size();
-	}
+  @XmlElement(name = "Name")
+  protected String name;
 
-	@SuppressWarnings("unused")
-    @XmlElement(name = "NbContrasts")
-	private int nbContrasts;
-	
-	public int getNbContrasts(){
-		return this.contrasts.size();
-	}
-	
-	@SuppressWarnings("unused")
-	private void setNbContrasts(int nbContrasts){
-		this.nbContrasts = nbContrasts;
-	}
-	
-	/**
-	 * Empty Constructor.
-	 */
-	public ContrastCollection() {
-		super();
-	}
-	
-	public ContrastCollection(String name,
-			List<Contrast> contrasts, int nbContrasts) {
-		super();
-		this.name = name;
-		this.contrasts = contrasts;
-		this.nbContrasts = nbContrasts;
-	}
-	
-	public ContrastCollection(
-			List<Contrast> contrasts, int nbContrasts) {
-		super();
-		this.contrasts = contrasts;
-		this.nbContrasts = nbContrasts;
-	}
-	
-	public ContrastCollection(
-			List<Contrast> contrasts) {
-		super();
-		this.contrasts = contrasts;
-		this.nbContrasts = contrasts.size();
-	}
-	
-	@Override
-  public String toString(){
-		CharArrayWriter writer = new CharArrayWriter();
-        this.marshall(writer);
-        return writer.toString();
-	}
-	
-	/**
-     * Charge les valeurs de contrastes decrites dans le fichier XML.
-     * Si le fichier n'existe pas, cree un nouveau fichier vide.
-     * @param fileName fichier XML decrivant le fichier à charger
-     * @return les valeurs de contrastes decrites dans le fichier XML
-     * 				ou un fihier vide si le fichier n'existe pas.
-     */
-    public static ContrastCollection unmarshall(String fileName) {
-    	logger.info("unmarshalling the contrast matrix");
-        try {
-            return unmarshall(new FileInputStream(fileName));
-        } catch (FileNotFoundException e) {
-            logger.error("File " + fileName + " could not be read");
-            return new ContrastCollection();
+  public String getName() {
+    return this.name;
+  }
+
+  @XmlElement(name = "Contrast")
+  private List<Contrast> contrasts = new ArrayList<Contrast>();
+
+  public List<Contrast> getContrasts() {
+    return this.contrasts;
+  }
+
+  public void setContrasts(List<Contrast> contrasts) {
+    this.contrasts = contrasts;
+    this.nbContrasts = contrasts.size();
+  }
+
+  @SuppressWarnings("unused")
+  @XmlElement(name = "NbContrasts")
+  private int nbContrasts;
+
+  public int getNbContrasts() {
+    return this.contrasts.size();
+  }
+
+  @SuppressWarnings("unused")
+  private void setNbContrasts(int nbContrasts) {
+    this.nbContrasts = nbContrasts;
+  }
+
+  /**
+   * Empty Constructor.
+   */
+  public ContrastCollection() {
+    super();
+  }
+
+  public ContrastCollection(String name, List<Contrast> contrasts,
+      int nbContrasts) {
+    super();
+    this.name = name;
+    this.contrasts = contrasts;
+    this.nbContrasts = nbContrasts;
+  }
+
+  public ContrastCollection(List<Contrast> contrasts, int nbContrasts) {
+    super();
+    this.contrasts = contrasts;
+    this.nbContrasts = nbContrasts;
+  }
+
+  public ContrastCollection(List<Contrast> contrasts) {
+    super();
+    this.contrasts = contrasts;
+    this.nbContrasts = contrasts.size();
+  }
+
+  @Override
+  public String toString() {
+    CharArrayWriter writer = new CharArrayWriter();
+    this.marshall(writer);
+    return writer.toString();
+  }
+
+  /**
+   * Charge les valeurs de contrastes decrites dans le fichier XML. Si le
+   * fichier n'existe pas, cree un nouveau fichier vide.
+   * @param fileName fichier XML decrivant le fichier à charger
+   * @return les valeurs de contrastes decrites dans le fichier XML ou un fihier
+   *         vide si le fichier n'existe pas.
+   */
+  public static ContrastCollection unmarshall(String fileName) {
+    logger.info("unmarshalling the contrast matrix");
+    try {
+      return unmarshall(new FileInputStream(fileName));
+    } catch (FileNotFoundException e) {
+      logger.error("File " + fileName + " could not be read");
+      return new ContrastCollection();
+    }
+  }
+
+  public static ContrastCollection unmarshall(InputStream stream) {
+    try {
+      JAXBContext context = JAXBContext.newInstance(ContrastCollection.class);
+      Unmarshaller m = context.createUnmarshaller();
+      ContrastCollection contrastcollection = (ContrastCollection) m
+          .unmarshal(stream);
+      return contrastcollection;
+    } catch (JAXBException e) {
+      e.printStackTrace();
+    }
+    return new ContrastCollection();
+  }
+
+  public void marshall(Writer writer) {
+    try {
+      JAXBContext context = JAXBContext.newInstance(ContrastCollection.class);
+      Marshaller m = context.createMarshaller();
+      m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+      m.marshal(this, writer);
+    } catch (JAXBException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void marshall(OutputStream stream) {
+    try {
+      JAXBContext context = JAXBContext.newInstance(ContrastCollection.class);
+      Marshaller m = context.createMarshaller();
+      m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+      m.marshal(this, stream);
+    } catch (JAXBException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Sauve le SLD dans le fichier en parametre
+   * @param fileName fichier dans lequel on sauve le SLD
+   */
+  public void marshall(String fileName) {
+    try {
+      this.marshall(new FileOutputStream(fileName));
+    } catch (FileNotFoundException e) {
+      logger.error("File " + fileName + " could not be written to"); //$NON-NLS-1$//$NON-NLS-2$
+    }
+  }
+
+  private static ContrastCollection cogitContrasts = null;
+
+  public static Contrast getCOGITContrast(ColorimetricColor c1,
+      ColorimetricColor c2) {
+    Contrast c = null;
+
+    if (cogitContrasts == null) {
+      cogitContrasts = ContrastCollection.unmarshall(ContrastCollection.class
+          .getClassLoader().getResourceAsStream("symbol/color/Contrast.xml"));
+    }
+    for (Contrast contrast : cogitContrasts.getContrasts()) {
+      if (contrast.getIdC1() == c1.getIdColor()) {
+        if (contrast.getIdC2() == c2.getIdColor()) {
+          c = contrast;
         }
+      }
     }
+    return c;
+  }
 
-    public static ContrastCollection unmarshall(InputStream stream) {
-        try {
-            JAXBContext context = JAXBContext.newInstance(
-            		ContrastCollection.class);
-            Unmarshaller m = context.createUnmarshaller();
-            ContrastCollection contrastcollection = (ContrastCollection) m
-            .unmarshal(stream);
-            return contrastcollection;
-        } catch (JAXBException e) {
-            e.printStackTrace();
+  public Contrast getCOGITContrastPerso(ColorimetricColor c1,
+      ColorimetricColor c2) {
+    Contrast c = null;
+    for (Contrast contrast : this.getContrasts()) {
+      if (contrast.getIdC1() == c1.getIdColor()) {
+        if (contrast.getIdC2() == c2.getIdColor()) {
+          c = contrast;
+          break;
         }
-        return new ContrastCollection();
+      }
     }
-    
-    public void marshall(Writer writer) {
-        try {
-            JAXBContext context = JAXBContext.newInstance(
-            		ContrastCollection.class);
-            Marshaller m = context.createMarshaller();
-            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            m.marshal(this, writer);
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void marshall(OutputStream stream) {
-        try {
-            JAXBContext context = JAXBContext.newInstance(
-            		ContrastCollection.class);
-            Marshaller m = context.createMarshaller();
-            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            m.marshal(this, stream);
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Sauve le SLD dans le fichier en parametre
-     * @param fileName fichier dans lequel on sauve le SLD
-     */
-    public void marshall(String fileName) {
-        try {
-            this.marshall(new FileOutputStream(fileName));
-        } catch (FileNotFoundException e) {
-            logger.error("File " + fileName + " could not be written to"); //$NON-NLS-1$//$NON-NLS-2$
-        }
-    }
-    
-    private static ContrastCollection cogitContrasts = null;
-    
-    public static Contrast getCOGITContrast(ColorimetricColor c1, ColorimetricColor c2) {
-    	Contrast c = null;
-    	
-    	if (cogitContrasts == null) {
-    		  cogitContrasts = ContrastCollection
-				.unmarshall(ContrastCollection.class.getResource("/symbol/color/Contrast.xml").getPath());
-    		 }
-    	for (Contrast contrast : cogitContrasts.getContrasts()) {
-			if (contrast.getIdC1() == c1.getIdColor()) {
-				if (contrast.getIdC2() == c2.getIdColor()) {
-					c = contrast;
-				}
-			}
-		}
-    	return c;
-    }
-    
-    public Contrast getCOGITContrastPerso(ColorimetricColor c1, ColorimetricColor c2){
-    	Contrast c = null;
-    	for (Contrast contrast : this.getContrasts()) {
-			if (contrast.getIdC1() == c1.getIdColor()) {
-				if (contrast.getIdC2() == c2.getIdColor()) {
-					c = contrast;
-					break;
-				}
-			}
-		}
-    	return c;
-    }
+    return c;
+  }
 }
