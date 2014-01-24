e86da02f5206ee0ce845958ba639437ead326c7d
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2014-01-24 16:34:35 +0000

    geoxygene bat install script

--- geoxygene-appli/install/geoxygene.bat
+++ geoxygene-appli/install/geoxygene.bat
@@ -3,24 +3,26 @@
 REM java -Xms1024m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=256m 
 REM      -Duser.language=en -jar "geoxygene-appli-1.6.jar" 
 
-REM set JAVA="java"
+REM java -Xms1024m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=256m -Duser.language=en -cp "geoxygene-appli-1.6.jar;lib/*.jar" fr.ign.cogit.geoxygene.appli.GeOxygeneApplication
 
-REM set JAVA_MAXMEM="-Xms1024m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=256m"
-REM set GEOX_OPTS="-Duser.language=en"
-REM set GEOX_LIB=".\lib"
+set JAVA="java"
 
-REM set MAIN="fr.ign.cogit.geoxygene.appli.GeOxygeneApplication"
+set JAVA_MAXMEM="-Xms1024m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=256m"
+set GEOX_OPTS="-Duser.language=en"
+set GEOX_LIB=".\lib"
 
-REM set JARS=.
-REM set CLASSPATH=
-REM for %%i in (.\lib\*.jar) do call cpappend.bat %%i
-REM set CLASSPATH=%JARS%;./conf;geoxygene-appli-1.6.jar;
+set MAIN="fr.ign.cogit.geoxygene.appli.GeOxygeneApplication"
+
+set JARS=.
+set CLASSPATH=
+for %%i in (.\lib\*.jar) do call cpappend.bat %%i
+set CLASSPATH=%JARS%;./conf;geoxygene-appli-1.6.jar;
 REM echo %CLASSPATH%
  
-REM %JAVA% "%JAVA_MAXMEM%" "%GEOX_OPTS%" -cp %CLASSPATH% fr.ign.cogit.geoxygene.appli.GeOxygeneApplication
+%JAVA% "%JAVA_MAXMEM%" "%GEOX_OPTS%" -cp %CLASSPATH% fr.ign.cogit.geoxygene.appli.GeOxygeneApplication
 
 
 
-java -Xms1024m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=256m -Duser.language=en -cp "geoxygene-appli-1.6.jar;lib/*.jar" fr.ign.cogit.geoxygene.appli.GeOxygeneApplication
+
 
 
