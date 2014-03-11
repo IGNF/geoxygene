5e1b1315ec159602313158af9879f21896a2121d
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2014-03-11 16:06:13 +0000

    integration test in osm module

---
+++ geoxygene-osm/src/integration-test/java/fr/ign/cogit/osm/load/ITLoadFile.java
@@ -0,0 +1,35 @@
+package fr.ign.cogit.osm.load;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fr.ign.cogit.geoxygene.osm.importexport.OSMLoader;
+import fr.ign.cogit.geoxygene.osm.importexport.OsmDataset;
+
+/**
+ * 
+ * @author MDVan-Damme
+ */
+public class ITLoadFile {
+  
+  @Test
+  public void BasicAndLightFileLoadTest() {
+    try {
+      OsmDataset dataset = new OsmDataset();
+      Runnable fillLayersTask = null;
+      OSMLoader loader = new OSMLoader(new File(ITLoadFile.class.getClassLoader().getResource("map.osm").getPath()), dataset, fillLayersTask, "2154");
+      
+      // loader.importOsmData();
+      Assert.assertEquals(0, loader.getNbNoeuds());
+ 
+    } catch(Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+}
---
+++ geoxygene-osm/src/test/java/fr/ign/cogit/osm/importexport/OSMLoaderTest.java
@@ -0,0 +1,13 @@
+package fr.ign.cogit.osm.importexport;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OSMLoaderTest {
+  
+  @Test
+  public void importOsmDataTest() {
+    Assert.assertTrue(true);
+  }
+
+}
