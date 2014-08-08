e52a652702ccf2f5241cb690499e15633a07604f
Adrien Maudet <adrien.maudet@ign.fr>
2014-08-08 14:42:39 +0000

    Get SVG document method added.

--- geoxygene-style/src/main/java/fr/ign/cogit/geoxygene/style/ExternalGraphic.java
+++ geoxygene-style/src/main/java/fr/ign/cogit/geoxygene/style/ExternalGraphic.java
@@ -21,7 +21,6 @@ package fr.ign.cogit.geoxygene.style;
 
 import java.awt.Image;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -47,114 +46,120 @@ import org.w3c.dom.svg.SVGDocument;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ExternalGraphic {
-	
-	/** LOGGER. */
-    private final static Logger LOGGER = Logger.getLogger(ExternalGraphic.class.getName());
-	
-    @XmlElement(name = "href")
-    private String href;
 
-    /**
-     * Renvoie la valeur de l'attribut href.
-     * 
-     * @return la valeur de l'attribut href
-     */
-    public String getHref() {
-        return this.href;
-    }
+  /** LOGGER. */
+  private final static Logger LOGGER = Logger.getLogger(ExternalGraphic.class
+      .getName());
 
-    /**
-     * Affecte la valeur de l'attribut href.
-     * 
-     * @param href
-     *            l'attribut href à affecter
-     */
-    public void setHref(String href) {
-        this.href = href;
-    }
+  @XmlElement(name = "href")
+  private String href;
 
-    @XmlElement(name = "Format")
-    private String format;
+  /**
+   * Renvoie la valeur de l'attribut href.
+   * 
+   * @return la valeur de l'attribut href
+   */
+  public String getHref() {
+    return this.href;
+  }
 
-    /**
-     * Renvoie la valeur de l'attribut format.
-     * 
-     * @return la valeur de l'attribut format
-     */
-    public String getFormat() {
-        return this.format;
-    }
+  /**
+   * Affecte la valeur de l'attribut href.
+   * 
+   * @param href l'attribut href à affecter
+   */
+  public void setHref(String href) {
+    this.href = href;
+  }
 
-    /**
-     * Affecte la valeur de l'attribut format.
-     * 
-     * @param format
-     *            l'attribut format à affecter
-     */
-    public void setFormat(String format) {
-        this.format = format;
-    }
+  @XmlElement(name = "Format")
+  private String format;
 
-    @XmlTransient
-    private Image onlineResource = null;
+  /**
+   * Renvoie la valeur de l'attribut format.
+   * 
+   * @return la valeur de l'attribut format
+   */
+  public String getFormat() {
+    return this.format;
+  }
 
-    /**
-     * Renvoie la valeur de l'attribut onlineResource.
-     * 
-     * @return la valeur de l'attribut onlineResource
-     */
-    public Image getOnlineResource() {
-        if (this.onlineResource == null) {
-            try {
-                URL url = ExternalGraphic.class.getResource(this.href);
-                if (url == null) {
-                    url = new URL(this.href);
-                }
-                LOGGER.trace("try to read '" + url + "'");
-                this.onlineResource = ImageIO.read(url);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+  /**
+   * Affecte la valeur de l'attribut format.
+   * 
+   * @param format l'attribut format à affecter
+   */
+  public void setFormat(String format) {
+    this.format = format;
+  }
+
+  @XmlTransient
+  private Image onlineResource = null;
+
+  /**
+   * Renvoie la valeur de l'attribut onlineResource.
+   * 
+   * @return la valeur de l'attribut onlineResource
+   */
+  public Image getOnlineResource() {
+    if (this.onlineResource == null) {
+      try {
+        URL url = ExternalGraphic.class.getResource(this.href);
+        if (url == null) {
+          url = new URL(this.href);
         }
-        return this.onlineResource;
+        LOGGER.trace("try to read '" + url + "'");
+        this.onlineResource = ImageIO.read(url);
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
+    return this.onlineResource;
+  }
 
-    public GraphicsNode getGraphicsNode() {
-        try {
-            String xmlParser = XMLResourceDescriptor.getXMLParserClassName();
-            SAXSVGDocumentFactory df = new SAXSVGDocumentFactory(xmlParser);
-            SVGDocument doc = null;
-            if (this.href != null) {
-                if (!this.href.substring(0, 4).equalsIgnoreCase("file")) {
-                    doc = df.createSVGDocument(ExternalGraphic.class.getResource(this.href).toString());
-                } else {
-                    doc = df.createSVGDocument(this.href);
-                }
-            }
-            UserAgent userAgent = new UserAgentAdapter();
-            DocumentLoader loader = new DocumentLoader(userAgent);
-            BridgeContext ctx = new BridgeContext(userAgent, loader);
-            ctx.setDynamicState(BridgeContext.DYNAMIC);
-            GVTBuilder builder = new GVTBuilder();
-            GraphicsNode rootGN = builder.build(ctx, doc);
-            return rootGN;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+  public GraphicsNode getGraphicsNode() {
+
+    SVGDocument doc = this.getSVGDocument();
+    if (doc == null)
+      return null;
+    UserAgent userAgent = new UserAgentAdapter();
+    DocumentLoader loader = new DocumentLoader(userAgent);
+    BridgeContext ctx = new BridgeContext(userAgent, loader);
+    ctx.setDynamicState(BridgeContext.DYNAMIC);
+    GVTBuilder builder = new GVTBuilder();
+    GraphicsNode rootGN = builder.build(ctx, doc);
+    return rootGN;
+  }
+
+  public SVGDocument getSVGDocument() {
+    String xmlParser = XMLResourceDescriptor.getXMLParserClassName();
+    SAXSVGDocumentFactory df = new SAXSVGDocumentFactory(xmlParser);
+    SVGDocument doc = null;
+    try {
+      if (this.href != null) {
+        if (!this.href.substring(0, 4).equalsIgnoreCase("file")) {
+
+          doc = df.createSVGDocument(ExternalGraphic.class.getResource(
+              this.href).toString());
+
+        } else {
+          doc = df.createSVGDocument(this.href);
         }
-        return null;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return doc;
+  }
 
-    /**
-     * Affecte la valeur de l'attribut onlineResource.
-     * 
-     * @param onlineResource
-     *            l'attribut onlineResource à affecter
-     */
-    public void setOnlineResource(Image onlineResource) {
-        this.onlineResource = onlineResource;
-    }
+  /**
+   * Affecte la valeur de l'attribut onlineResource.
+   * 
+   * @param onlineResource l'attribut onlineResource à affecter
+   */
+  public void setOnlineResource(Image onlineResource) {
+    this.onlineResource = onlineResource;
+  }
 }
