901e58fb8db53c41353f95b632a76c378a98da7c
Jérémie Turbet <Jeremie.Turbet@mail.com>
2014-11-04 13:11:18 +0000

    GL screenspace & text shaders

---
+++ geoxygene-appli/src/main/resources/shaders/text-layer.frag.glsl
@@ -0,0 +1,22 @@
+#version 400
+
+uniform sampler2D colorTexture2;
+uniform float globalOpacity = 1;
+uniform float objectOpacity = 1;
+uniform int antialiasingSize = 1;
+uniform int screenWidth;
+uniform int screenHeight;
+
+in vec4 fragmentColor;
+in vec2 fragmentTextureCoord;
+
+out vec4 outColor;
+
+vec4 colorFilter( vec4 col );
+
+void main(void) {
+	vec4 textureColor =  texture(colorTexture2, fragmentTextureCoord );
+//    outColor = vec4( fragmentTextureCoord * vec2(0.333) + textureColor.rg * 0.666, textureColor.b, 0.5 );
+//    outColor = vec4( fragmentTextureCoord, 0.0, 1.0 );
+	outColor = textureColor;
+}
---
+++ geoxygene-appli/src/main/resources/shaders/text-screenspace.vert.glsl
@@ -0,0 +1,15 @@
+#version 400 core
+
+layout (location = 0) in vec3 vertexPosition;
+layout (location = 1) in vec2 vertexTextureCoord;
+layout (location = 2) in vec4 vertexColor;
+uniform int antialiasingSize = 1;
+
+out vec4 fragmentColor;
+out vec2 fragmentTextureCoord;
+
+void main(void) {
+	gl_Position = vec4( vertexPosition, 1.0 );
+	fragmentColor = vertexColor;
+	fragmentTextureCoord = vertexTextureCoord;
+}
