f4530eba31ac6253786ddef372dfe4ffee683feb
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2014-11-24 15:31:05 +0000

    move the two modules "geoxygene-geopensim" et "geoxygene-pea-rep" out of geoxygene kernel

--- geoxygene-geopensim/src/main/resources/sql/postgis/geopensim/correction_apres_import.sql
+++
@@ -1,4 +0,0 @@
-update batiment set datesource = 2002 where datesource = 1998;
-update vegetation set datesource = 2002 where datesource = 1998;
-update parking set datesource = 2002 where datesource = 1998;
-update terrain_sport set datesource = 2002 where datesource = 1998;
--- geoxygene-pea-rep/src/main/java/fr/ign/cogit/cartagen/pearep/derivation/mraware/MultiRepAwareStrategy.java
+++
@@ -1,5 +0,0 @@
-package fr.ign.cogit.cartagen.pearep.derivation.mraware;
-
-public enum MultiRepAwareStrategy {
-  POST_PROC, BALANCED_POST_PROC, PRE_FILTERING, PROC_CHANGE;
-}
