6c5a5ad940888fa8278cde557082fa7f3ace0427
Guillaume Touya <guillaume.touya@ign.fr>
2015-01-28 14:58:42 +0000

    A new method for clutter/legibility evaluation for (Olsson et al 2011)

--- geoxygene-carto/src/main/java/fr/ign/cogit/carto/evaluation/clutter/RasterClutterMethod.java
+++ geoxygene-carto/src/main/java/fr/ign/cogit/carto/evaluation/clutter/RasterClutterMethod.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * This software is released under the licence CeCILL
+ * 
+ * see Licence_CeCILL-C_fr.html see Licence_CeCILL-C_en.html
+ * 
+ * see <a href="http://www.cecill.info/">http://www.cecill.info/a>
+ * 
+ * @copyright IGN
+ ******************************************************************************/
 package fr.ign.cogit.carto.evaluation.clutter;
 
 import java.awt.image.BufferedImage;
@@ -13,79 +22,84 @@ import javax.media.jai.TiledImage;
 
 public class RasterClutterMethod {
 
-  private TiledImage image;
+	private TiledImage image;
 
-  public RasterClutterMethod(TiledImage image) {
-    super();
-    this.image = image;
-  }
+	public RasterClutterMethod(TiledImage image) {
+		super();
+		this.image = image;
+	}
 
-  public TiledImage getImage() {
-    return image;
-  }
+	public TiledImage getImage() {
+		return image;
+	}
 
-  public void setImage(TiledImage image) {
-    this.image = image;
-  }
+	public void setImage(TiledImage image) {
+		this.image = image;
+	}
 
-  /**
-   * According to (Rosenhotlz et al 2007), a simple edge density gives a good
-   * global evaluation of an image clutter. The edge detection method is the
-   * default JAI one, i.e. Sobel gradient magnitude.
-   * @return
-   */
-  public double getEdgeDensityClutter() {
-    return computeEdgeDensityClutter(image);
-  }
+	/**
+	 * According to (Rosenhotlz et al 2007), a simple edge density gives a good
+	 * global evaluation of an image clutter. The edge detection method is the
+	 * default JAI one, i.e. Sobel gradient magnitude.
+	 * 
+	 * @return
+	 */
+	public double getEdgeDensityClutter() {
+		return computeEdgeDensityClutter(image);
+	}
 
-  /**
-   * Compute the edge density clutter in a grid version of the image, where the
-   * number of columns of the grid is given (the size of the cells is computed
-   * according to the image size).
-   * @param nbColumns
-   * @return
-   */
-  public Map<Integer, Map<Integer, Double>> getGridEdgeDensityClutter(
-      int nbColumns) {
-    // first compute the number of pixels in a grid cell
-    int cellSize = image.getWidth() / nbColumns;
-    int nbRows = Math.round(image.getHeight() / cellSize);
-    Map<Integer, Map<Integer, Double>> clutters = new HashMap<Integer, Map<Integer, Double>>();
-    int y = 0;
-    for (int i = 0; i < nbRows; i++) {
-      int x = 0;
-      Map<Integer, Double> columns = new HashMap<Integer, Double>();
-      for (int j = 0; j < nbColumns; j++) {
-        // compute the sub-image in the i,j cell
-        TiledImage subImage = image.getSubImage(x, y, cellSize, cellSize);
-        columns.put(j, computeEdgeDensityClutter(subImage));
-        x += cellSize;
-      }
-      clutters.put(i, columns);
-      y += cellSize;
-    }
+	/**
+	 * Compute the edge density clutter in a grid version of the image, where
+	 * the number of columns of the grid is given (the size of the cells is
+	 * computed according to the image size).
+	 * 
+	 * @param nbColumns
+	 * @return
+	 */
+	public Map<Integer, Map<Integer, Double>> getGridEdgeDensityClutter(
+			int nbColumns) {
+		// first compute the number of pixels in a grid cell
+		int cellSize = image.getWidth() / nbColumns;
+		int nbRows = Math.round(image.getHeight() / cellSize);
+		Map<Integer, Map<Integer, Double>> clutters = new HashMap<Integer, Map<Integer, Double>>();
+		int y = 0;
+		for (int i = 0; i < nbRows; i++) {
+			int x = 0;
+			Map<Integer, Double> columns = new HashMap<Integer, Double>();
+			for (int j = 0; j < nbColumns; j++) {
+				// compute the sub-image in the i,j cell
+				TiledImage subImage = image.getSubImage(x, y, cellSize,
+						cellSize);
+				columns.put(j, computeEdgeDensityClutter(subImage));
+				x += cellSize;
+			}
+			clutters.put(i, columns);
+			y += cellSize;
+		}
 
-    return clutters;
-  }
+		return clutters;
+	}
 
-  private double computeEdgeDensityClutter(PlanarImage image) {
-    // first compute the RenderedOp that computes both vertical and horizontal
-    // edge detection
-    KernelJAI sobelVertKernel = KernelJAI.GRADIENT_MASK_SOBEL_VERTICAL;
-    KernelJAI sobelHorizKernel = KernelJAI.GRADIENT_MASK_SOBEL_HORIZONTAL;
-    ParameterBlock pb = new ParameterBlock();
-    pb.addSource(image);
-    pb.add(sobelHorizKernel);
-    pb.add(sobelVertKernel);
-    RenderedOp renderedOp = JAI.create("gradientmagnitude", pb);
-    BufferedImage edgeImage = renderedOp.getAsBufferedImage();
+	private double computeEdgeDensityClutter(PlanarImage image) {
+		// first compute the RenderedOp that computes both vertical and
+		// horizontal
+		// edge detection
+		KernelJAI sobelVertKernel = KernelJAI.GRADIENT_MASK_SOBEL_VERTICAL;
+		KernelJAI sobelHorizKernel = KernelJAI.GRADIENT_MASK_SOBEL_HORIZONTAL;
+		ParameterBlock pb = new ParameterBlock();
+		pb.addSource(image);
+		pb.add(sobelHorizKernel);
+		pb.add(sobelVertKernel);
+		RenderedOp renderedOp = JAI.create("gradientmagnitude", pb);
+		BufferedImage edgeImage = renderedOp.getAsBufferedImage();
 
-    // then compute a density value, i.e. the mean
-    int edgeTotal = 0;
-    for (int i = 0; i < edgeImage.getWidth(); i++)
-      for (int j = 0; j < edgeImage.getHeight(); j++)
-        edgeTotal += Math.abs(edgeImage.getRGB(i, j));
+		// then compute a density value, i.e. the mean
+		int edgeTotal = 0;
+		for (int i = 0; i < edgeImage.getWidth(); i++)
+			for (int j = 0; j < edgeImage.getHeight(); j++)
+				edgeTotal += Math.abs(edgeImage.getRGB(i, j));
 
-    return Math.abs(edgeTotal / (edgeImage.getWidth() * edgeImage.getHeight()));
-  }
+		return Math.abs(edgeTotal
+				/ (edgeImage.getWidth() * edgeImage.getHeight()));
+	}
 }
