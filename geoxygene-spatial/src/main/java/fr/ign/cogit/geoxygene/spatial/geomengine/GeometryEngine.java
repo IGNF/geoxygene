733da23e9febfc515b13821a03a8e7280553fc43
Imran Lokhat <lartane@gmail.com>
2015-03-11 13:35:06 +0000

    adding GeometryEngine, which produces a geometryfactory based on JTS (JTSGeomFactory)

---
+++ geoxygene-spatial/src/main/java/fr/ign/cogit/geoxygene/spatial/geomengine/GeometryEngine.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * This software is released under the licence CeCILL
+ * 
+ * see Licence_CeCILL-C_fr.html see Licence_CeCILL-C_en.html
+ * 
+ * see <a href="http://www.cecill.info/">http://www.cecill.info/a>
+ * 
+ * @copyright IGN
+ ******************************************************************************/
+package fr.ign.cogit.geoxygene.spatial.geomengine;
+
+import fr.ign.cogit.geoxygene.api.spatial.geomengine.AbstractGeometryEngine;
+import fr.ign.cogit.geoxygene.spatial.JTSGeomFactory;
+
+public class GeometryEngine extends AbstractGeometryEngine {
+
+  static {
+    setInstance(new GeometryEngine());
+  }
+
+  private GeometryEngine() {
+    switch (getGeometrie()) {
+      case "JTS":
+        setFactory(new JTSGeomFactory());
+        break;
+      case "SOCLE":
+      default:
+        System.out.println("not implemented -- reverting to JTS");
+        setFactory(new JTSGeomFactory());
+        break;
+    }
+  }
+
+  // method to get the static block fired
+  public static void init() {
+    System.out.println(GeometryEngine.class.getName() + " initialized");
+  }
+}
