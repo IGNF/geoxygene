9bdd4dd6e93f10ec1db4801fe29467677df0b353
Imran Lokhat <lartane@gmail.com>
2015-03-11 11:10:08 +0000

    adding AbstractGeometryEngine, abstract class for "geometry engines", which produce a geometry factory)

---
+++ geoxygene-api/src/main/java/fr/ign/cogit/geoxygene/api/spatial/geomengine/AbstractGeometryEngine.java
@@ -0,0 +1,38 @@
+package fr.ign.cogit.geoxygene.api.spatial.geomengine;
+
+import fr.ign.cogit.geoxygene.api.spatial.AbstractGeomFactory;
+
+public abstract class AbstractGeometryEngine {
+  private static AbstractGeometryEngine instance;
+  private static AbstractGeomFactory factory;
+  private static String geometrie = "JTS";
+
+  public static AbstractGeometryEngine getInstance() {
+    return instance;
+  }
+
+  protected static void setInstance(AbstractGeometryEngine instance) {
+    AbstractGeometryEngine.instance = instance;
+  }
+
+  public static String getGeometrie() {
+    return AbstractGeometryEngine.geometrie;
+  }
+
+  public static void setGeometrie(String geometrie) {
+    AbstractGeometryEngine.geometrie = geometrie;
+  }
+
+  public static AbstractGeomFactory getFactory() {
+    return factory;
+  }
+
+  protected static void setFactory(AbstractGeomFactory factory) {
+    AbstractGeometryEngine.factory = factory;
+  };
+
+  // to be overriden in children
+  // protected static void init() {
+  // }
+
+}
