282cbb845b5a3b13b971012986cdab18e99ca602
Adrien Maudet <adrien.maudet@ign.fr>
2015-05-29 11:19:39 +0000

    change visibility of private method to protected

--- geoxygene-spatial/src/main/java/fr/ign/cogit/geoxygene/generalisation/simplification/SimplificationAlgorithm.java
+++ geoxygene-spatial/src/main/java/fr/ign/cogit/geoxygene/generalisation/simplification/SimplificationAlgorithm.java
@@ -84,8 +84,8 @@ public class SimplificationAlgorithm {
    * @param cpPlusCourt
    * @return
    */
-  private static resultatSuppressionCotePolygone suppressionCote(IPolygon poly,
-      PolygonSegment cotePolygone) {
+  protected static resultatSuppressionCotePolygone suppressionCote(
+      IPolygon poly, PolygonSegment cotePolygone) {
 
     // recupere l'anneau dont on souhaite supprimer un cote
     IRing anneau;
