cd743af542506fdbb06a2da000af1c2d50a67f3c
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2015-08-25 16:14:44 +0000

    Cartogram with Dougenik algorithm + plugin to see an example

---
+++ geoxygene-appli/src/main/java/fr/ign/cogit/geoxygene/appli/example/CartogramDougenikPlugin.java
@@ -0,0 +1,169 @@
+package fr.ign.cogit.geoxygene.appli.example;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+
+import javax.swing.JMenu;
+
+import org.apache.log4j.Logger;
+
+import fr.ign.cogit.geoxygene.api.feature.IFeature;
+import fr.ign.cogit.geoxygene.api.feature.IPopulation;
+import fr.ign.cogit.geoxygene.appli.FloatingProjectFrame;
+import fr.ign.cogit.geoxygene.appli.GeOxygeneApplication;
+import fr.ign.cogit.geoxygene.appli.plugin.AbstractGeOxygeneApplicationPlugin;
+import fr.ign.cogit.geoxygene.contrib.algorithms.CartogramDougenik;
+import fr.ign.cogit.geoxygene.feature.DefaultFeature;
+import fr.ign.cogit.geoxygene.feature.Population;
+import fr.ign.cogit.geoxygene.style.Layer;
+import fr.ign.cogit.geoxygene.style.PolygonSymbolizer;
+import fr.ign.cogit.geoxygene.util.conversion.ShapefileReader;
+
+/**
+ * CartogramDougenikPlugin show Dougenik algorithm
+ * The plugin use it with Paris arrondissements and recensement attribute.
+ */
+public class CartogramDougenikPlugin extends AbstractGeOxygeneApplicationPlugin {
+  
+  /** Logger. */
+  static final Logger LOGGER = Logger.getLogger(CartogramDougenikPlugin.class.getName());
+  
+  // private static final String NOM_FICHIER = "ContourCommune75.shp";
+  // private static final String NOM_ATTRIBUT = "POP";
+  // private static final String NOM_FICHIER = "Cantons.shp";
+  // private static final String NOM_ATTRIBUT = "Pop2006";
+  private static final String NOM_FICHIER = "prov_4326_simple.shp";
+  private static final String NOM_ATTRIBUT = "lonnom";
+  // private static final String NOM_ATTRIBUT = "nb";
+  
+  @Override
+  public void initialize(final GeOxygeneApplication application) {
+    this.application = application;
+
+    JMenu menuAwt = addMenu("Example", "Cartogram by Dougenik");
+    application.getMainFrame().getMenuBar()
+      .add(menuAwt, application.getMainFrame().getMenuBar().getMenuCount() - 2);
+    
+  }
+  
+  @Override
+  public void actionPerformed(ActionEvent evt) {
+    
+    try {
+      
+      URL urlContourCommune75 = new URL("file", "", "./data/cartogram/" + NOM_FICHIER);
+      IPopulation<IFeature> popCommune = ShapefileReader.read(urlContourCommune75.getFile());
+      
+      // IPopulation<IFeature> popCommune = createPop();
+      
+      // Pop début
+      FloatingProjectFrame popCommuneDebutProjectFrame = (FloatingProjectFrame) this.application.getMainFrame().getSelectedProjectFrame();
+      Layer layer1 = popCommuneDebutProjectFrame.addUserLayer(popCommune, "Original", null);
+      PolygonSymbolizer ps1 = (PolygonSymbolizer) layer1.getSymbolizer();
+      ps1.setUnitOfMeasurePixel();
+      ps1.getStroke().setStrokeWidth(1);
+      ps1.getStroke().setStrokeOpacity(0.8f);
+      ps1.getStroke().setStroke(new Color(0, 144, 135));
+      ps1.getFill().setColor(new Color(163, 217, 90));
+      ps1.getFill().setFillOpacity(0.8f);
+      
+      // Pop fin
+      int nbIteration = 8;
+      Population<DefaultFeature> entrees1 = CartogramDougenik.doCartogram(popCommune, NOM_ATTRIBUT, nbIteration);
+      FloatingProjectFrame popCommuneFinProjectFrame1 = (FloatingProjectFrame) this.application.getMainFrame().newProjectFrame();
+      Layer layer2 = popCommuneFinProjectFrame1.addUserLayer(entrees1, "Iteration 8", null);
+      PolygonSymbolizer ps2 = (PolygonSymbolizer) layer2.getSymbolizer();
+      ps2.setUnitOfMeasurePixel();
+      ps2.getStroke().setStrokeWidth(1);
+      ps2.getStroke().setStrokeOpacity(0.8f);
+      ps2.getStroke().setStroke(new Color(0, 144, 135));
+      ps2.getFill().setColor(new Color(255, 247, 125));
+      ps2.getFill().setFillOpacity(0.8f);
+      
+      // Pop fin
+      nbIteration = 12;
+      Population<DefaultFeature> entrees2 = CartogramDougenik.doCartogram(popCommune, NOM_ATTRIBUT, nbIteration);
+      FloatingProjectFrame popCommuneFinProjectFrame2 = (FloatingProjectFrame) this.application.getMainFrame().newProjectFrame();
+      Layer layer3 = popCommuneFinProjectFrame2.addUserLayer(entrees2, "Iteration 12", null);
+      PolygonSymbolizer ps3 = (PolygonSymbolizer) layer3.getSymbolizer();
+      ps3.setUnitOfMeasurePixel();
+      ps3.getStroke().setStrokeWidth(1);
+      ps3.getStroke().setStrokeOpacity(0.8f);
+      ps3.getStroke().setStroke(new Color(0, 144, 135));
+      ps3.getFill().setColor(new Color(255, 247, 125));
+      ps3.getFill().setFillOpacity(0.8f);
+      
+      nbIteration = 20;
+      Population<DefaultFeature> entrees3 = CartogramDougenik.doCartogram(popCommune, NOM_ATTRIBUT, nbIteration);
+      FloatingProjectFrame popCommuneFinProjectFrame3 = (FloatingProjectFrame) this.application.getMainFrame().newProjectFrame();
+      Layer layer4 = popCommuneFinProjectFrame3.addUserLayer(entrees3, "Iteration 20", null);
+      PolygonSymbolizer ps4 = (PolygonSymbolizer) layer4.getSymbolizer();
+      ps4.setUnitOfMeasurePixel();
+      ps4.getStroke().setStrokeWidth(1);
+      ps4.getStroke().setStrokeOpacity(0.8f);
+      ps4.getStroke().setStroke(new Color(0, 144, 135));
+      ps4.getFill().setColor(new Color(255, 247, 125));
+      ps4.getFill().setFillOpacity(0.8f);
+      
+      // Organize
+      this.application.getMainFrame().organizeCurrentDesktop();
+      popCommuneDebutProjectFrame.getLayerViewPanel().getViewport().zoomToFullExtent();
+      popCommuneDebutProjectFrame.getLayerViewPanel().getViewport().zoomOut();
+      popCommuneFinProjectFrame1.getLayerViewPanel().getViewport().zoomToFullExtent();
+      popCommuneFinProjectFrame1.getLayerViewPanel().getViewport().zoomOut();
+      popCommuneFinProjectFrame2.getLayerViewPanel().getViewport().zoomToFullExtent();
+      popCommuneFinProjectFrame2.getLayerViewPanel().getViewport().zoomOut();
+      popCommuneFinProjectFrame3.getLayerViewPanel().getViewport().zoomToFullExtent();
+      popCommuneFinProjectFrame3.getLayerViewPanel().getViewport().zoomOut();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    LOGGER.trace("End rendering cartogram with Dougenik algorithm.");
+      
+  }
+  
+  /*private IPopulation<IFeature> createPop() {
+    IPopulation<IFeature> popCarre = new Population<IFeature>("Carre");
+    
+    DefaultFeature carre1 = new DefaultFeature(new GM_Polygon(new GM_Envelope(10, 20, 10, 40)));
+    DefaultFeature carre2 = new DefaultFeature(new GM_Polygon(new GM_Envelope(30, 40, 10, 20)));
+    DefaultFeature carre3 = new DefaultFeature(new GM_Polygon(new GM_Envelope(30, 40, 30, 40)));
+    
+    FeatureType carreFeatureType = new FeatureType();
+    carreFeatureType.setTypeName("Metadonnee");
+    carreFeatureType.setGeometryType(IPolygon.class);
+    
+    AttributeType idTextNature = new AttributeType("nb", "double");
+    carreFeatureType.addFeatureAttribute(idTextNature);
+    
+    // Création d'un schéma associé au featureType
+    SchemaDefaultFeature schema = new SchemaDefaultFeature();
+    schema.setFeatureType(carreFeatureType);
+    carreFeatureType.setSchema(schema);
+    
+    Map<Integer, String[]> attLookup = new HashMap<Integer, String[]>(0);
+    attLookup.put(new Integer(0), new String[] { "nb", "nb" });
+    schema.setAttLookup(attLookup);
+    
+    popCarre.setFeatureType(carreFeatureType);
+    
+    Object[] attributes = new Object[] { "10" };
+    carre1.setSchema(schema);
+    carre1.setAttributes(attributes);
+    attributes = new Object[] { "40" };
+    carre2.setSchema(schema);
+    carre2.setAttributes(attributes);
+    attributes = new Object[] { "20" };
+    carre3.setSchema(schema);
+    carre3.setAttributes(attributes);
+    
+    popCarre.add(carre1);
+    popCarre.add(carre2);
+    popCarre.add(carre3);
+    
+    return popCarre;
+  }*/
+
+}
---
+++ geoxygene-contrib/src/main/java/fr/ign/cogit/geoxygene/contrib/algorithms/CartogramDougenik.java
@@ -0,0 +1,259 @@
+package fr.ign.cogit.geoxygene.contrib.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import fr.ign.cogit.geoxygene.api.feature.IFeature;
+import fr.ign.cogit.geoxygene.api.feature.IPopulation;
+import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IDirectPosition;
+import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IPolygon;
+import fr.ign.cogit.geoxygene.api.spatial.geomaggr.IMultiSurface;
+import fr.ign.cogit.geoxygene.feature.DefaultFeature;
+import fr.ign.cogit.geoxygene.feature.Population;
+import fr.ign.cogit.geoxygene.feature.SchemaDefaultFeature;
+import fr.ign.cogit.geoxygene.schema.schemaConceptuelISOJeu.FeatureType;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPosition;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPositionList;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_LineString;
+import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_Polygon;
+import fr.ign.cogit.geoxygene.spatial.geomaggr.GM_MultiSurface;
+import fr.ign.cogit.geoxygene.spatial.geomprim.GM_Point;
+
+/**
+ * <p>
+ * An Algorithm to Construct Continuous Area Cartograms:
+ *      <a href='http://lambert.nico.free.fr/tp/biblio/Dougeniketal1985.pdf'>Dougeniketal1985.pdf</a>
+ * </p>
+ */
+public class CartogramDougenik {
+  
+  /** LOGGER. */
+  private final static Logger LOGGER = Logger.getLogger(CartogramDougenik.class.getName());
+  
+  /**
+   * 
+   * @param population
+   * @param attributeName : needs to be double type
+   * @param nbIteration
+   * @return
+   */
+  public static Population<DefaultFeature> doCartogram(IPopulation<IFeature> population, String attributeName, int nbIteration) {
+    
+    // Define a new population 
+    Population<DefaultFeature> newPopulation = new Population<DefaultFeature>(false, "CartogramDougenik", DefaultFeature.class, true);
+    newPopulation.setFeatureType((FeatureType) population.getFeatureType());
+    ((FeatureType)newPopulation.getFeatureType()).setSchema((SchemaDefaultFeature) ((FeatureType)population.getFeatureType()).getSchema());
+    
+    for (IFeature feature : population) {
+      DefaultFeature n = newPopulation.nouvelElement(feature.getGeom());
+      n.setSchema((SchemaDefaultFeature) ((FeatureType)newPopulation.getFeatureType()).getSchema());
+      n.setAttributes(((DefaultFeature)feature).getAttributes());
+    }
+    
+    int totalValue = 0;
+    for (IFeature feature : newPopulation) {
+      int value = new Integer(feature.getAttribute(attributeName).toString());
+      if (value > totalValue) {
+        totalValue = value;
+      }
+    }
+    LOGGER.trace("POP MAX = " + totalValue);
+    
+    // ----------------------------------------
+    // For each iteration (user controls when done)
+    int targetSizeError = 1;
+    for (int i = 0; i < nbIteration; i++) {
+
+      // ----------------------------------------
+      //    Calculate area et centroid
+      double totalArea = 0.0;
+      
+      for (IFeature feature : newPopulation) {
+        double area = feature.getGeom().area();
+        totalArea = totalArea + area;
+        
+      }
+      
+      // ----------------------------------------
+      //    radius, mass, error size
+      Map<IFeature, IDirectPosition> centroidList = new HashMap<IFeature, IDirectPosition>();
+      Map<IFeature, Double> radiusList = new HashMap<IFeature, Double>();
+      Map<IFeature, Double> desiredList = new HashMap<IFeature, Double>();
+      Map<IFeature, Double> massList = new HashMap<IFeature, Double>();
+      Map<IFeature, Double> sizeErrorList = new HashMap<IFeature, Double>();
+      for (IFeature feature : newPopulation) {
+        int value = new Integer(feature.getAttribute(attributeName).toString());
+        double area = feature.getGeom().area();
+
+        centroidList.put(feature, feature.getGeom().centroid());
+        double desired = totalArea * value / totalValue;
+        desiredList.put(feature, desired);
+        radiusList.put(feature, Math.sqrt(area / Math.PI));
+        massList.put(feature, Math.sqrt(desired/Math.PI) - Math.sqrt(area/Math.PI));
+        sizeErrorList.put(feature,  Math.max(area, desired) / Math.min(area, desired));
+      }
+      
+      // ----------------------------------------
+      //    ForceReductionFactor
+      double sizeError = 0;
+      double forceReductionFactor = 0;
+      for (double d : sizeErrorList.values()) {
+        sizeError += d;
+      }
+      sizeError = sizeError / sizeErrorList.size();
+      forceReductionFactor = 1 / (1 + sizeError);
+      
+      // ----------------------------------------
+      // For each boundary line (i.e. for each polygon)
+      for (IFeature feature : newPopulation) {
+        
+        // ----------------------------------------
+        // CAS POLYGON
+        if (feature.getGeom() instanceof GM_Polygon) {
+          
+          // Remplace polygon par cette nouvelle liste de points
+          DirectPositionList posList = new DirectPositionList();
+          
+          // For each coordinate pair (i.e. for each edge)
+          int cpt = 0;
+          IDirectPosition coordOld = null;
+          for (IDirectPosition coord : feature.getGeom().coord()) {
+            
+            if (cpt > 0) {
+              
+              GM_LineString line = new GM_LineString (coordOld, coord);
+              
+              // pour chaque point
+              for (int k = 0; k < line.getControlPoint().size(); k++) { 
+                GM_Point point = new GM_Point(line.getControlPoint().get(k));
+                
+                double x = point.getPosition().getX();
+                double x0 = x;
+                double y = point.getPosition().getY();
+                double y0 = y;
+                
+                // For each polygon centroid, create an  array of vectors: [x, y]
+                for (IFeature currentFeature : centroidList.keySet()) {
+                  
+                  IDirectPosition centre = centroidList.get(currentFeature);
+                  double cx = centre.getX();
+                  double cy = centre.getY();
+                  double radius = radiusList.get(currentFeature);
+                  double mass = massList.get(currentFeature);
+                  double distance = Math.sqrt(Math.pow(x0 - cx, 2) + Math.pow(y0 - cy, 2));
+                  double fij = 0;
+                  if (distance > radius) {
+                    fij = mass * radius / distance;
+                  } else {
+                    double xF = distance / radius;
+                    fij = mass * (Math.pow(xF, 2) * (4 - 3 * xF));
+                  }
+                  fij = fij * forceReductionFactor / distance;
+                  
+                  x = (x0 - cx) * fij + x;
+                  y = (y0 - cy) * fij + y;
+                  
+                }
+                
+                if (k > 0 || (k == 0 && cpt == 1)) {
+                  posList.add(new DirectPosition(x, y));
+                }
+                
+              }
+                
+            }
+            
+            cpt++;
+            coordOld = coord;
+          }
+          
+          feature.setGeom(new GM_Polygon(new GM_LineString(posList)));
+          // System.out.println("geom transformée.");
+        
+          // end instance of polygon
+        } else if (feature.getGeom() instanceof GM_MultiSurface) {
+          
+          GM_MultiSurface<GM_Polygon> multiSurface = (GM_MultiSurface<GM_Polygon>) feature.getGeom();
+          IMultiSurface<IPolygon> unionGeom = new GM_MultiSurface<IPolygon>();
+          
+          for (int j = 0; j < multiSurface.size(); j++) {
+            
+            // GM_Polygon polygon = multiSurface.get(j);
+            
+            // Remplace polygon par cette nouvelle liste de points
+            DirectPositionList posList = new DirectPositionList();
+            
+            // For each coordinate pair (i.e. for each edge)
+            int cpt = 0;
+            IDirectPosition coordOld = null;
+            for (IDirectPosition coord : multiSurface.get(j).coord()) {
+              if (cpt > 0) {
+                GM_LineString line = new GM_LineString (coordOld, coord);
+              
+                // pour chaque point
+                for (int k = 0; k < line.getControlPoint().size(); k++) { 
+                  GM_Point point = new GM_Point(line.getControlPoint().get(k));
+                  
+                  double x = point.getPosition().getX();
+                  double x0 = x;
+                  double y = point.getPosition().getY();
+                  double y0 = y;
+                  
+                  // For each polygon centroid, create an  array of vectors: [x, y]
+                  for (IFeature currentFeature : centroidList.keySet()) {
+                    
+                    IDirectPosition centre = centroidList.get(currentFeature);
+                    double cx = centre.getX();
+                    double cy = centre.getY();
+                    double radius = radiusList.get(currentFeature);
+                    double mass = massList.get(currentFeature);
+                    double distance = Math.sqrt(Math.pow(x0 - cx, 2) + Math.pow(y0 - cy, 2));
+                    double fij = 0;
+                    if (distance > radius) {
+                      fij = mass * radius / distance;
+                    } else {
+                      double xF = distance / radius;
+                      fij = mass * (Math.pow(xF, 2) * (4 - 3 * xF));
+                    }
+                    fij = fij * forceReductionFactor / distance;
+                    
+                    x = (x0 - cx) * fij + x;
+                    y = (y0 - cy) * fij + y;
+                    
+                  }
+                  
+                  if (k > 0 || (k == 0 && cpt == 1)) {
+                    posList.add(new DirectPosition(x, y));
+                  }
+                  
+                }
+                
+              }
+            
+              cpt++;
+              coordOld = coord;
+            }
+          
+            unionGeom.add(new GM_Polygon(new GM_LineString(posList)));
+          }
+          feature.setGeom(unionGeom);
+          
+        }
+        
+      } // end each boundary line 
+      
+      // Break if we hit the target size error
+      if (sizeError <= targetSizeError) break;
+      
+    } // end each iteration
+    
+    return newPopulation;
+    
+  }
+  
+  
+}
+
+
