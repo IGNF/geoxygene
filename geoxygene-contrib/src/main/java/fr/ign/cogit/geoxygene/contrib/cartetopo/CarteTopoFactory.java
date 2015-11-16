58bef61bc35b84802454834b84fdd60e15031e67
Imran Lokhat <lartane@gmail.com>
2015-11-16 16:09:23 +0000

    new Factory method taking 2 FeatureCollection as input (can be used if we want to add the border of the extent as linestrings in the second collection for example)

--- geoxygene-contrib/src/main/java/fr/ign/cogit/geoxygene/contrib/cartetopo/CarteTopoFactory.java
+++ geoxygene-contrib/src/main/java/fr/ign/cogit/geoxygene/contrib/cartetopo/CarteTopoFactory.java
@@ -14,8 +14,7 @@ import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_LineString;
  * @author Julien Perret
  */
 public class CarteTopoFactory {
-  public static Logger logger = Logger.getLogger(CarteTopoFactory.class
-      .getName());
+  public static Logger logger = Logger.getLogger(CarteTopoFactory.class.getName());
 
   /**
    * Create a topological map from a feature collection, a name and a threshold.
@@ -30,8 +29,7 @@ public class CarteTopoFactory {
    * @param threshold threshold used for all the things mentioned above
    * @return a topological map
    */
-  public static CarteTopo newCarteTopo(String name,
-      IFeatureCollection<? extends IFeature> collection, double threshold,
+  public static CarteTopo newCarteTopo(String name, IFeatureCollection<? extends IFeature> collection, double threshold,
       boolean infiniteFace) {
     // Initialisation d'une nouvelle CarteTopo
     CarteTopo carteTopo = new CarteTopo(name);
@@ -79,8 +77,7 @@ public class CarteTopoFactory {
    * @param collection collection de features
    * @return a topological map
    */
-  public static CarteTopo newCarteTopo(String name,
-      IFeatureCollection<? extends IFeature> collection) {
+  public static CarteTopo newCarteTopo(String name, IFeatureCollection<? extends IFeature> collection) {
     return CarteTopoFactory.newCarteTopo(name, collection, 1.0, false);
   }
 
@@ -93,8 +90,69 @@ public class CarteTopoFactory {
    * @param collection collection de features
    * @return a topological map
    */
-  public static CarteTopo newCarteTopo(
-      IFeatureCollection<? extends IFeature> collection) {
+  public static CarteTopo newCarteTopo(IFeatureCollection<? extends IFeature> collection) {
     return CarteTopoFactory.newCarteTopo("TopoMap", collection); //$NON-NLS-1$
   }
+
+  /**
+   * Create a topological map from a feature collection, a name and a threshold.
+   * The threshold is used for node creation as well as for topology and planar
+   * map creations.
+   * <p>
+   * Création d'une CarteTopo à partir de deux FT_FeatureCollection, d'un nom et
+   * d'un seuil. Ce dernier sert à créer les noeuds manquants, la topologie
+   * arc-noeud et pour rendre le graphe planaire.
+   * @param name name of the topo map
+   * @param collection collection de features
+   * @param threshold threshold used for all the things mentioned above
+   * @return a topological map
+   */
+  public static CarteTopo newCarteTopo(String name, IFeatureCollection<? extends IFeature> collection,
+      IFeatureCollection<? extends IFeature> collection2, double threshold, boolean infiniteFace) {
+    // Initialisation d'une nouvelle CarteTopo
+    CarteTopo carteTopo = new CarteTopo(name);
+    carteTopo.setBuildInfiniteFace(infiniteFace);
+    // Récupération des arcs de la carteTopo
+    IPopulation<Arc> arcs = carteTopo.getPopArcs();
+    // Import des arcs de la collection dans la carteTopo
+    for (IFeature feature : collection) {
+      // création d'un nouvel élément
+      Arc arc = arcs.nouvelElement();
+      // affectation de la géométrie de l'objet issu de la collection
+      // à l'arc de la carteTopo
+      arc.setGeometrie((GM_LineString) feature.getGeom());
+      // instanciation de la relation entre l'arc créé et l'objet
+      // issu de la collection
+      arc.addCorrespondant(feature);
+    }
+    for (IFeature feature : collection2) {
+      // création d'un nouvel élément
+      Arc arc = arcs.nouvelElement();
+      // affectation de la géométrie de l'objet issu de la collection
+      // à l'arc de la carteTopo
+      arc.setGeometrie((GM_LineString) feature.getGeom());
+      // instanciation de la relation entre l'arc créé et l'objet
+      // issu de la collection
+      arc.addCorrespondant(feature);
+    }
+
+    CarteTopoFactory.logger.info("Nombre d'arcs créés : " //$NON-NLS-1$
+        + carteTopo.getPopArcs().size());
+    // Création des noeuds manquants
+    CarteTopoFactory.logger.info("Création des noeuds manquants"); //$NON-NLS-1$
+    carteTopo.creeNoeudsManquants(threshold);
+    // Création de la topologie Arcs Noeuds
+    CarteTopoFactory.logger.info("Création de la topologie Arcs Noeuds"); //$NON-NLS-1$
+    carteTopo.creeTopologieArcsNoeuds(threshold);
+    // La carteTopo est rendue planaire
+    CarteTopoFactory.logger.info("La carte topologique est rendue planaire"); //$NON-NLS-1$
+    carteTopo.rendPlanaire(threshold);
+    CarteTopoFactory.logger.info("Création des faces de la carte topologique"); //$NON-NLS-1$
+    // Création des faces de la carteTopo
+    carteTopo.creeTopologieFaces();
+    CarteTopoFactory.logger.info("Nombre de faces créées : " //$NON-NLS-1$
+        + carteTopo.getPopFaces().size());
+    return carteTopo;
+  }
+
 }
