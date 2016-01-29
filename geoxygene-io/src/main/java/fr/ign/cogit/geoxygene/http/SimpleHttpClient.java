b0ab8c67a07456b8c4a92c2aa43765a6fcb8c552
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2016-01-29 09:12:16 +0000

    Add different contentType to choose in SimpleHttpClient.connect method

--- geoxygene-io/src/main/java/fr/ign/cogit/geoxygene/http/SimpleHttpClient.java
+++ geoxygene-io/src/main/java/fr/ign/cogit/geoxygene/http/SimpleHttpClient.java
@@ -36,7 +36,7 @@ import java.net.URL;
  * Simple HTTP Client class.
  */
 public class SimpleHttpClient {
-  
+
   /**
    * The URL we want to connect
    */
@@ -46,70 +46,76 @@ public class SimpleHttpClient {
    * The HTTP connection
    */
   protected HttpURLConnection server;
-  
+
   /**
    * Constructor.
    * <p>
    * 
-   * @param szUrl
-   *            : The URL we want to connect.
+   * @param szUrl : The URL we want to connect.
    */
   public SimpleHttpClient(String szUrl) throws Exception {
-      try {
-          url = new URL(szUrl);
-      } catch (Exception e) {
-          throw new Exception("Invalid URL");
-      }
+    try {
+      url = new URL(szUrl);
+    } catch (Exception e) {
+      throw new Exception("Invalid URL");
+    }
   }
-  
+
   /**
    * Connect the client to it's URL by using the specified method.
    * 
-   * @param method
-   *            : String object for client method (POST, GET,...)
+   * @param method : String object for client method (POST, GET,...)
    */
   public void connect(String method) throws Exception {
-      try {
-          server = (HttpURLConnection) url.openConnection();
-          server.setDoInput(true);
-          server.setDoOutput(true);
-          server.setRequestMethod(method);
-          // server.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
-          server.setRequestProperty("Content-type", "text/xml");
-          server.connect();
-      } catch (Exception e) {
-          throw new Exception("Connection failed");
-      }
-  }
-  
-  public void connectProxyIGN(String method) throws Exception {
     try {
-      
-        // Set IGN Proxy
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.ign.fr", 3128));
-        
-        server = (HttpURLConnection) url.openConnection(proxy);
-        server.setDoInput(true);
-        server.setDoOutput(true);
-        server.setRequestMethod(method);
-        // server.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
-        server.setRequestProperty("Content-type", "text/xml");
-        server.connect();
+      server = (HttpURLConnection) url.openConnection();
+      server.setDoInput(true);
+      server.setDoOutput(true);
+      server.setRequestMethod(method);
+      // server.setRequestProperty("Content-type",
+      // "application/x-www-form-urlencoded");
+      // server.setRequestProperty("Content-type", "text/xml");
+      server.setRequestProperty("Content-Type", "application/json");
+      server.connect();
     } catch (Exception e) {
-        throw new Exception("Connection failed");
+      throw new Exception("Connection failed");
     }
   }
-  
-  
 
-  
+  public void connectProxyIGN(String method) throws Exception {
+    connectProxyIGN(method, "application/x-www-form-urlencoded");
+  }
+
+  /**
+   * 
+   * @param method
+   * @param type : "application/json", "application/x-www-form-urlencoded",
+   *          "text/xml", ....
+   * @throws Exception
+   */
+  public void connectProxyIGN(String method, String contentType) throws Exception {
+    try {
+      // Set IGN Proxy
+      Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.ign.fr", 3128));
+
+      server = (HttpURLConnection) url.openConnection(proxy);
+      server.setDoInput(true);
+      server.setDoOutput(true);
+      server.setRequestMethod(method);
+      server.setRequestProperty("Content-Type", contentType);
+      server.connect();
+    } catch (Exception e) {
+      throw new Exception("Connection failed");
+    }
+  }
+
   /**
    * Disconnect the client.
    */
   public void disconnect() {
-      server.disconnect();
+    server.disconnect();
   }
-  
+
   /**
    * Read the response from the server.
    * 
@@ -117,56 +123,57 @@ public class SimpleHttpClient {
    * @throws Exception
    */
   public String getResponse() throws Exception {
-      StringBuffer response = new StringBuffer();
+    StringBuffer response = new StringBuffer();
+    try {
+      // BufferedReader s = new BufferedReader(new
+      // InputStreamReader(server.getInputStream()));
+      BufferedReader s = new BufferedReader(new InputStreamReader(server.getInputStream()));
+      String line = s.readLine();
+      while (line != null) {
+        response.append(line);
+        line = s.readLine();
+      }
+      s.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new Exception("Unable to read input stream");
+    }
+    return response.toString();
+  }
 
-      try {
-          BufferedReader s = new BufferedReader(new InputStreamReader(server.getInputStream()));
-          String line = s.readLine();
-          while (line != null) {
-              response.append(line);
-              line = s.readLine();
-          }
-          s.close();
-      } catch (Exception e) {
-          throw new Exception("Unable to read input stream");
-      }
-      return response.toString();
-  }
-  
   public void getResponseInFile(String filePath) throws Exception {
-      try {
-          
-    	  InputStream instream = server.getInputStream();
-    	  BufferedInputStream bis = new BufferedInputStream(instream);
-    	  BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
-    	  int inByte;
-    	  while ((inByte = bis.read()) != -1 ) {
-    		  bos.write(inByte);
-    	  }
-    	  bis.close();
-    	  bos.close();
-    	  
-      } catch (Exception e) {
-          throw new Exception("Unable to read input stream");
+    try {
+
+      InputStream instream = server.getInputStream();
+      BufferedInputStream bis = new BufferedInputStream(instream);
+      BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
+      int inByte;
+      while ((inByte = bis.read()) != -1) {
+        bos.write(inByte);
       }
+      bis.close();
+      bos.close();
+
+    } catch (Exception e) {
+      throw new Exception("Unable to read input stream");
+    }
   }
-  
+
   /**
    * Submit a POST to the server.
    * 
-   * @param content
-   *            The content of the POST message
+   * @param content The content of the POST message
    * @throws Exception
    */
   public void post(String content) throws Exception {
-      try {
-          BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
-          bw.write(content, 0, content.length());
-          bw.flush();
-          bw.close();
-      } catch (Exception e) {
-          throw new Exception("Unable to write to output stream");
-      }
+    try {
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+      bw.write(content, 0, content.length());
+      bw.flush();
+      bw.close();
+    } catch (Exception e) {
+      throw new Exception("Unable to write to output stream");
+    }
   }
 
 }
