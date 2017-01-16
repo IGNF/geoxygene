9c72e75eac1cd15bc4352e56f6a0059bc21ab46e
quythytruong <truong.quythy@gmail.com>
2017-01-16 15:41:01 +0100

    Imports Pbf files into a PostGIS database

---
+++ geoxygene-osm/src/main/java/fr/ign/cogit/geoxygene/osm/importexport/pbf/GeoxPbfReader.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * This software is released under the licence CeCILL
+ * 
+ * see Licence_CeCILL-C_fr.html see Licence_CeCILL-C_en.html
+ * 
+ * see <a href="http://www.cecill.info/">http://www.cecill.info/a>
+ * 
+ * @copyright IGN
+ ******************************************************************************/
+package fr.ign.cogit.geoxygene.osm.importexport.pbf;
+
+import java.io.File;
+
+import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader;
+
+public class GeoxPbfReader {
+
+    public static GeoxSink mySink = new GeoxSink();
+
+    public static void main(String[] args) {
+        long tdeb = System.currentTimeMillis();
+        PbfReader reader = new PbfReader(
+                new File("D:/Users/qttruong/workspace/paris_20150121.osm.pbf"),
+                1);
+        reader.setSink(mySink);
+        System.out.println("on entre dans le run");
+        reader.run();
+        if (mySink.relValues.length() > 0) {
+            mySink.relValues.deleteCharAt(mySink.relValues.length() - 1);
+            mySink.myQueries
+                    .append("INSERT INTO relation (idrel, id, uid, vrel,changeset,username, datemodif, tags) VALUES ")
+                    .append(mySink.relValues).append(";");
+            // System.out.println("Requête\n" + mySink.myQueries.toString());
+            // mySink.executeQuery();
+            mySink.relValues.setLength(0);
+        }
+        System.out.println("Durée traitement : "
+                + ((System.currentTimeMillis() - tdeb) / 1000 / 60) + " min");
+    }
+}
