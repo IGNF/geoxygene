64b5b26fbf4ea180b1084a7ed75341b478a7e197
Mickaël Brasebin <mickael.brasebin@ign.fr>
2018-10-19 15:09:59 +0200

    Update of OBB

--- geoxygene-sig3d/src/main/java/fr/ign/cogit/geoxygene/sig3d/calculation/parcelDecomposition/OBBBlockDecomposition.java
+++ geoxygene-sig3d/src/main/java/fr/ign/cogit/geoxygene/sig3d/calculation/parcelDecomposition/OBBBlockDecomposition.java
@@ -21,8 +21,9 @@ import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPosition;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPositionList;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_LineString;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_Polygon;
+import fr.ign.cogit.geoxygene.util.attribute.AttributeManager;
+import fr.ign.cogit.geoxygene.util.conversion.ShapefileReader;
 import fr.ign.cogit.geoxygene.util.conversion.ShapefileWriter;
-import fr.ign.cogit.geoxygene.util.conversion.WktGeOxygene;
 
 /**
  * Re-implementation of block decomposition into parcels from :
@@ -40,27 +41,58 @@ import fr.ign.cogit.geoxygene.util.conversion.WktGeOxygene;
 public class OBBBlockDecomposition {
 
 	public static void main(String[] args) throws Exception {
-		
-		DirectPosition.PRECISION = 5;
-		
-		String strMuliPol = "POLYGON ((930504.39 6676077.2 0.0, 930503.2 6676071.07 0.0, 930485.03 6676097.67 0.0, 930481.32 6676103.72 0.0, 930462.54 6676132.06 0.0, 930462.63 6676145.02 0.0, 930462.79 6676160.69 0.0, 930459.84 6676170.01 0.0, 930444.52 6676187.57 0.0, 930440.01 6676199.36 0.0, 930439.81 6676199.93 0.0, 930436.99 6676204.35 0.0, 930426.35 6676221.02 0.0, 930425.36 6676236.96 0.0, 930420.83 6676241.63 0.0, 930404.88 6676255.86 0.0, 930406.14 6676257.45 0.0, 930394.8 6676266.84 0.0, 930394.22 6676267.19 0.0, 930398.09 6676276.79 0.0, 930399.56 6676279.36 0.0, 930403.84 6676284.62 0.0, 930407.5 6676287.92 0.0, 930411.9 6676291.22 0.0, 930415.44 6676293.44 0.0, 930419.13 6676295.19 0.0, 930421.32 6676295.78 0.0, 930427.86 6676296.75 0.0, 930430.31 6676296.72 0.0, 930432.45 6676296.1 0.0, 930437.31 6676292.66 0.0, 930434.51 6676294.18 0.0, 930431.87 6676294.79 0.0, 930429.37 6676294.73 0.0, 930427.65 6676294.39 0.0, 930410.69 6676281.4 0.0, 930450.57 6676247.86 0.0, 930448.48 6676245.5 0.0, 930428.87 6676232.34 0.0, 930428.38 6676230.74 0.0, 930429.18 6676221.98 0.0, 930429.85 6676220.53 0.0, 930441.14 6676201.64 0.0, 930447.02 6676189.03 0.0, 930468.95 6676163.75 0.0, 930469.99 6676168.82 0.0, 930477.41 6676196.71 0.0, 930483.7 6676222.35 0.0, 930484.44 6676225.73 0.0, 930496.78 6676276.86 0.0, 930505.27 6676278.61 0.0, 930504.84 6676276.93 0.0, 930503.29 6676275.93 0.0, 930496.08 6676246.39 0.0, 930488.4 6676214.93 0.0, 930487.78 6676212.38 0.0, 930488.11 6676212.27 0.0, 930481.71 6676187.51 0.0, 930476.29 6676166.56 0.0, 930475.36 6676158.67 0.0, 930473.02 6676131.37 0.0, 930491.7 6676099.55 0.0, 930504.39 6676077.2 0.0))";
 
-        double roadEpsilon = 0;
-        double noise = 0;
-        double maximalArea = 1200;
-        double maximalWidth = 50;
+		// Precision si set
+		DirectPosition.PRECISION = 4;
 
-        
-		
-		IPolygon pol = (IPolygon) FromGeomToSurface.convertGeom(WktGeOxygene.makeGeOxygene(strMuliPol)).get(0);
-		System.out.println(pol.toString());
-		
-		OBBBlockDecomposition decomposition = new OBBBlockDecomposition(pol, maximalArea, maximalWidth,  roadEpsilon);
-		IFeatureCollection<IFeature> featColl = decomposition.decompParcel(noise);
-		
-	
-		
-		ShapefileWriter.write(featColl, "/tmp/tmp.shp");
+		// Input 1/ the input shapes to split
+		String inputShapeFile = "/home/mbrasebin/Bureau/misc/shapes_final.shp";
+
+		// The output file that will contain all the decompositions
+		String shapeFileOut = "/home/mbrasebin/Bureau/misc/out.shp";
+
+		// Reading collection
+		IFeatureCollection<IFeature> featColl = ShapefileReader.read(inputShapeFile);
+
+		// Maxmimal area for a parcel
+		double maximalArea = 500;
+		// MAximal with to the road
+		double maximalWidth = 20;
+		// Do we want noisy results
+		double noise = 0;
+		// The with of the road that is created
+		double roadWidth = 3;
+
+		IFeatureCollection<IFeature> featCollOut = new FT_FeatureCollection<>();
+
+		int count = featColl.size();
+		// For each shape
+		for (int i=3500; i < count ; i++) {
+			IFeature feat = featColl.get(i);
+			System.out.println(i + " / " + featColl.size());
+
+			List<IOrientableSurface> surfaces = FromGeomToSurface.convertGeom(feat.getGeom());
+
+			if (surfaces.size() != 1) {
+				System.out.println("Not simple geometry : " + feat.toString());
+				continue;
+			}
+
+			// We run the algorithm of decomposition
+			OBBBlockDecomposition ffd = new OBBBlockDecomposition((IPolygon) surfaces.get(0), maximalArea, maximalWidth,
+					roadWidth);
+			IFeatureCollection<IFeature> results = ffd.decompParcel(noise);
+			
+			final int intCurrentCount = i;
+			results.stream().forEach(x -> AttributeManager.addAttribute(x, "ID", intCurrentCount, "Integer"));
+
+			// Get the results
+			featCollOut.addAll(results);
+
+		}
+
+		ShapefileWriter.write(featCollOut, shapeFileOut);
+
 	}
 
 	private double maximalArea, maximalWidth;
@@ -103,20 +135,19 @@ public class OBBBlockDecomposition {
 	private IFeatureCollection<IFeature> decompParcel(IPolygon p, double noise) throws Exception {
 
 		IFeatureCollection<IFeature> featCollOut = new FT_FeatureCollection<>();
-		
-		if( ! p .isValid()){
-			p= (IPolygon) p.buffer(0);
-			
-			if(! p.isValid()){
-				p= (IPolygon) p.buffer(0.001);
-			}else{
-				if(! p.isValid()){
+
+		if (!p.isValid()) {
+			p = (IPolygon) p.buffer(0);
+
+			if (!p.isValid()) {
+				p = (IPolygon) p.buffer(0.001);
+			} else {
+				if (!p.isValid()) {
 					System.out.println("Invalid polygon : " + p);
 					System.out.println("Try maybe with less precision : DirectPosition.Precision = 4");
 				}
 			}
-		
-			
+
 		}
 
 		double area = p.area();
@@ -182,7 +213,7 @@ public class OBBBlockDecomposition {
 
 		ILineString ext = new GM_LineString(this.polygonInit.getExterior().coord());
 
-		return (p.buffer(0.1)).intersection(ext).length();
+		return (p.buffer(0.2)).intersection(ext).length();
 	}
 
 	/**
@@ -215,14 +246,12 @@ public class OBBBlockDecomposition {
 		double alphaY = (0.5 - Math.random()) * noiseTemp;
 		IDirectPosition translateCentroid = new DirectPosition(centroid.getX() + alphaX, centroid.getY() + alphaY);
 
-
 		// Determine the points that intersect the line and the OBB according to
 		// chosen direction
 		// This points will be used for splitting
 		IDirectPositionList intersectedPoints = determineIntersectedPoints(
 				new LineEquation(translateCentroid, splitDirection),
 				(shortDirectionSplit) ? oBB.getLongestEdges() : oBB.getShortestEdges());
-
 
 		// Construction of the two splitting polygons by using the OBB edges and the
 		// intersection points
@@ -292,6 +321,12 @@ public class OBBBlockDecomposition {
 		IDirectPosition dp2 = eq
 				.intersectionLineLine(new LineEquation(ls.get(1).coord().get(0), ls.get(1).coord().get(1)));
 
+		if(dp1 == null) {
+			System.out.println("Null");
+			dp1 = eq
+					.intersectionLineLine(new LineEquation(ls.get(0).coord().get(0), ls.get(0).coord().get(1)));
+			
+		}
 		IDirectPositionList dpl = new DirectPositionList();
 		dpl.add(dp1);
 		dpl.add(dp2);
